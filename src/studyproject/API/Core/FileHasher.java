6147cc7d57e723e3aa15c64cb69d58cd87986607
mkaspera <Michael.kaspera@gmx.de>
2016-08-25 20:12:47 +0200

    java : added fileHasher
    - class FileHasher has one public static function that returns the
    sha-256 hash for a given file

---
+++ src/studyproject/API/Core/FileHasher.java
@@ -0,0 +1,38 @@
+package studyproject.API.Core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FileHasher {
+	
+	private static final int buffersize = 16384; 
+
+	/**
+	 * Calculates and returns the hash for a given file
+	 * @param filename Path to the file
+	 * @return the sha-256 hash for the given file
+	 */
+	public static String getFileHash(String filename) throws IOException, NoSuchAlgorithmException{
+		int bytesRead;
+		byte[] buffer = new byte[buffersize];
+			MessageDigest hashSum = MessageDigest.getInstance("SHA-256");
+		try(FileInputStream in = new FileInputStream(new File(filename));
+		DigestInputStream digester = new DigestInputStream(in, hashSum)){
+			bytesRead = digester.read(buffer, 0, buffersize);
+			while(bytesRead == buffersize){
+				bytesRead = digester.read(buffer, 0, buffersize);
+			}
+			byte[] hash = hashSum.digest();
+			StringBuffer hexString = new StringBuffer();
+	    	for (int i=0;i<hash.length;i++) {
+	    		hexString.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
+	    	}
+	    	return hexString.toString();
+		}
+	}
+	
+}
