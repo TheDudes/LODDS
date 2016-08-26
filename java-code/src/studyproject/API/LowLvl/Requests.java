114f4e3cf375ceaa49ed96c94e78511b09ec5600
L1nc0ln <Lincoln-park@gmx.de>
2016-08-26 17:11:07 +0200

    doc: added javadoc to Requests.java

--- java-code/src/studyproject/API/LowLvl/Requests.java
+++ java-code/src/studyproject/API/LowLvl/Requests.java
@@ -10,6 +10,14 @@ public class Requests {
 	private static final String GET_FILE = "get file ";
 	private static final String GET_SEND_PERMISSION = "get send-permission ";
 
+	/**
+	 * Requests an updated fileInfo list from the other client, the timestamp is the
+	 * time of last synchronization. If the timestamp is 0 the command is a
+	 * get_all command
+	 * @param socketStream the stream to write to
+	 * @param timestamp time of last synchronization, if 0 the client has to send a full list of all files it has
+	 * @return 0 or an error value
+	 */
 	public static int getInfoUp(BufferedOutputStream socketStream, long timestamp) {
 		try{
 			socketStream.write((GET_INFO_UP + timestamp + "\n").getBytes());
@@ -20,6 +28,15 @@ public class Requests {
 		return 0;
 	}
 
+	/**
+	 * Requests the transfer of a file from the other client. Can also request only parts of a file by
+	 * setting the start and/or endIndex to something else than 0 or the size of the file
+	 * @param socketStream the stream to write the request to
+	 * @param checksum the checksum of the requested file
+	 * @param startIndex the index from which the other client should start reading the file
+	 * @param endIndex the index at which the other client should stop reading the file
+	 * @return 0 or an error value
+	 */
 	public static int getFile(BufferedOutputStream socketStream, String checksum, long startIndex, long endIndex) {
 		try{
 			socketStream.write((GET_FILE + checksum + " " + startIndex + " " + endIndex + "\n").getBytes());
@@ -30,6 +47,11 @@ public class Requests {
 		return 0;
 	}
 
+	/**
+	 * Request the information about how much bytes the other client has still to send
+	 * @param socketStream the stream to write the request to
+	 * @return 0 or an error value
+	 */
 	public static int getInfoLoad(BufferedOutputStream socketStream) {
 		try{
 			socketStream.write((GET_INFO_LOAD).getBytes());
@@ -40,6 +62,15 @@ public class Requests {
 		return 0;
 	}
 
+	/**
+	 * Requests the send permission for a file by writing the arguments to the stream in the
+	 * way defined in the specification
+	 * @param socketStream the stream to write to
+	 * @param size the size of the file that permission to send is asked for
+	 * @param timeout the time until the request times out. Not a timestamp but a set amount of time in ms
+	 * @param fileName the name of the file that permission to send is asked for
+	 * @return 0 or an error value
+	 */
 	public static int getSendPermission(BufferedOutputStream socketStream, long size, long timeout, String fileName) {
 		try{
 			socketStream.write((GET_SEND_PERMISSION + size + " " + timeout + " " + fileName + "\n").getBytes());
