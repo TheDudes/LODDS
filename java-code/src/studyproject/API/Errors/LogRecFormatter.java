f2ae75791bf9fd5983b0daa9b8439b278e4fbcc7
Nintinugga <ninti@192.168.2.109>
2016-08-26 16:43:36 +0200

    java: Added LogRecFormatter.java
    - Formatter class which formats a log record to the format: time:
    message

---
+++ java-code/src/studyproject/API/Errors/LogRecFormatter.java
@@ -0,0 +1,21 @@
+package studyproject.API.Errors;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+import studyproject.API.Core.Utils;
+
+public class LogRecFormatter extends Formatter {
+	private StringBuilder sb;
+
+	@Override
+	public String format(LogRecord record) {
+		sb = new StringBuilder();
+		//append formatted unix timestamp
+		sb.append(Utils.formatUnixTimestamp(record.getMillis()));
+		sb.append(": ");
+		sb.append(record.getMessage());
+		return sb.toString();
+	}
+
+}
