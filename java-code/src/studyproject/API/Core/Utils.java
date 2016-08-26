63b84b5f2cb5383446e666015e23d7965298c296
Nintinugga <ninti@192.168.2.109>
2016-08-26 16:39:42 +0200

    java: Added Method formatUnixTimestamp
    - takes a long unixSeconds and formats it with the format "HH:mm:ss" and
    returns it as String with the Timezone.getDefault

--- java-code/src/studyproject/API/Core/Utils.java
+++ java-code/src/studyproject/API/Core/Utils.java
@@ -2,44 +2,76 @@ package studyproject.API.Core;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Utils {
-	
+	public static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
 	/**
-     * reads the exact number of bytes specified from a InputStream to the specified buffer, no less
-     * @param inStream the stream to read from
-     * @param destination the byte array to copy the read bytes to
-     * @param off offset for the byte array, all data gets copied in the array after this index
-     * @param len the number of bytes to read and copy
-     * @return the number of bytes read (=len)
-     * @throws IOException
-     */
-	public static int readThisLength(InputStream inStream, byte[] destination, int off, int len) throws IOException{
+	 * reads the exact number of bytes specified from a InputStream to the
+	 * specified buffer, no less
+	 * 
+	 * @param inStream
+	 *            the stream to read from
+	 * 
+	 * @param destination
+	 *            the byte array to copy the read bytes to
+	 * 
+	 * @param off
+	 *            offset for the byte array, all data gets copied in the array
+	 *            after this index
+	 * 
+	 * @param len
+	 *            the number of bytes to read and copy
+	 * 
+	 * @return the number of bytes read (=len)
+	 * 
+	 * @throws IOException
+	 */
+
+	public static int readThisLength(InputStream inStream, byte[] destination, int off, int len) throws IOException {
 		int readBytes = 0;
-		if(off + len > destination.length){
-			throw new IndexOutOfBoundsException("Can not read the specified length from the stream since the offset plus "
-					+ "length is bigger than the provided array. Offset: " + off + ", len: " + len 
-					+ ", arraysize: " + destination.length);
+		if (off + len > destination.length) {
+			throw new IndexOutOfBoundsException(
+					"Can not read the specified length from the stream since the offset plus "
+							+ "length is bigger than the provided array. Offset: " + off + ", len: " + len
+							+ ", arraysize: " + destination.length);
 		}
-		while(readBytes < len){
+		while (readBytes < len) {
 			readBytes = readBytes + inStream.read(destination, off + readBytes, len - readBytes);
 		}
 		return readBytes;
 	}
-	
+
 	/**
 	 * 
-	 * @param source the array containing the wanted bytes
-	 * @param startIndex the first index, included in the returned byte array
-	 * @param endIndex the last index, included in the returned byte array
+	 * @param source
+	 *            the array containing the wanted bytes
+	 * @param startIndex
+	 *            the first index, included in the returned byte array
+	 * @param endIndex
+	 *            the last index, included in the returned byte array
 	 * @return a byte array containing the specified bytes
 	 */
-	public static byte[] getBytesFromTo(byte[] source, int startIndex, int endIndex){
+	public static byte[] getBytesFromTo(byte[] source, int startIndex, int endIndex) {
 		byte[] returnValue = new byte[endIndex - startIndex];
-		for(int index = 0; index < endIndex - startIndex; index++){
+		for (int index = 0; index < endIndex - startIndex; index++) {
 			returnValue[index] = source[index + startIndex];
 		}
 		return returnValue;
+	}
+
+	
+	public static String formatUnixTimestamp(long unixSeconds) {
+		// *1000 is to convert seconds to milliseconds
+		Date date = new Date(unixSeconds * 1000L);
+		// give a timezone reference
+		sdf.setTimeZone(TimeZone.getDefault());
+
+		return sdf.format(date);
+
 	}
 
 }
