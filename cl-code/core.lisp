0acf614336e565c2e01ebb1bd0e08fa5ceda862f
d4ryus <w.wackerbauer@yahoo.de>
2016-08-27 01:31:12 +0200

    lisp: removed core.lisp
    was a unused file, core code lives inside cl-code-core.lisp

--- cl-code/core.lisp
+++
@@ -1,8 +0,0 @@
-;;;; cl-code-core.lisp
-
-(in-package #:cl-code-core)
-
-(defun sha-256 (stream)
-  "generates sha-256 sum out of given stream"
-  (ironclad:byte-array-to-hex-string
-    (ironclad:digest-stream :sha256 stream)))
