40148502e1faebac5f70b107a2c6f3f5af41dd8c
L1nc0ln <Lincoln-park@gmx.de>
2016-08-26 12:53:07 +0200

    java: added class Broadcasts that handles everything with UDP Broadcasts
    - added class Broadcasts that handles everything with UDP Broadcasts,
    functions are getNetworkAddresses that returns all addresses of the
    local machine, sendBroadcast that sends an UDP broadcast to all machines
    in the local network and receiveBroadcast which returns the information
    contained in the next UDP packet it gets via the BroadcastInfo parameter

--- java-code/src/studyproject/API/LowLvl/Broadcast.java
+++ java-code/src/studyproject/API/LowLvl/Broadcast.java
@@ -9,9 +9,14 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 
+import studyproject.API.Core.BroadcastInfo;
+import studyproject.API.Core.Utils;
+
 public class Broadcast {
 	
-	private static int port = 9002;
+	private static final int PORT = 9002;
+	private static final int PORT_SENDING = 9003;
+	private static final int BUFFERSIZE = 2048;
 
 	public static int getNetworkAddresses(ArrayList<String> networkAddresses){
 		try{
@@ -31,11 +36,13 @@ public class Broadcast {
 	
 	public static int sendBroadcast(String broadcastAddress, String networkAddress, int ipPort, long timestamp, String name){
 	    DatagramPacket packet;
-	    try(DatagramSocket socket = new DatagramSocket(port, InetAddress.getByName(broadcastAddress))){
+	    try(DatagramSocket socket = new DatagramSocket(PORT_SENDING, InetAddress.getByName(networkAddress))){
 	    	socket.setBroadcast(true);
 	    	byte[] packetContents = (networkAddress + " " + ipPort + " " + timestamp + " " + name + "\n").getBytes();
-	    	packet = new DatagramPacket(packetContents, 0, InetAddress.getByName(broadcastAddress), port);
+	    	packet = new DatagramPacket(packetContents, packetContents.length,
+	    			InetAddress.getByName(broadcastAddress), PORT);
 	    	socket.send(packet);
+	    	//socket.disconnect();
 	    } catch(IOException e){
 	    	//TODO real error codes
 	    	return -1;
@@ -43,4 +50,32 @@ public class Broadcast {
 	    return 0;
 	}
 	
+	
+	public static int receiveBroadcast(String broadcastAddress, BroadcastInfo broadcastInfo){
+		try(DatagramSocket socket = new DatagramSocket(PORT, InetAddress.getByName(broadcastAddress))){
+			DatagramPacket packet = new DatagramPacket(new byte[BUFFERSIZE], BUFFERSIZE);
+			socket.receive(packet);
+			int index = 0;
+			while(packet.getData()[index] != '\n'){
+				index++;
+				if(!(index < BUFFERSIZE)){
+					return -2;
+				}
+			}
+			String packetContents = new String(Utils.getBytesFromTo(packet.getData(), 0, index), "UTF-8");
+			String[] packetParts = packetContents.split(" ");
+			if(packetParts.length != 4){
+				//TODO real error codes
+				return -2;
+			}
+			broadcastInfo.networkAddress = packetParts[0];
+			broadcastInfo.ipPort = Integer.parseInt(packetParts[1]);
+			broadcastInfo.timestamp = Long.parseLong(packetParts[2]);
+			broadcastInfo.name = packetParts[3];
+		} catch(IOException e){
+			//TODO real error codes
+			return -1;
+		}
+		return 0;
+	}
 }
