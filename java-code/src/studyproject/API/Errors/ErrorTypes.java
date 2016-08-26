f7947598c24fc0666a249267c85441a52530de07
Nintinugga <ninti@192.168.2.109>
2016-08-26 18:35:40 +0200

    java: Added mapping of Enums to ints and method valueOf(int)

--- java-code/src/studyproject/API/Errors/ErrorTypes.java
+++ java-code/src/studyproject/API/Errors/ErrorTypes.java
@@ -1,5 +1,21 @@
 package studyproject.API.Errors;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum ErrorTypes {
-	IO, blub, blah
+	IO, blub, blah;
+	
+
+    private static Map<Integer, ErrorTypes> map = new HashMap<Integer, ErrorTypes>();
+
+    static {
+        for (ErrorTypes error : ErrorTypes.values()) {
+            map.put(error.ordinal(), error);
+        }
+    }
+
+    public static ErrorTypes valueOf(int error) {
+        return map.get(error);
+    }
 }
