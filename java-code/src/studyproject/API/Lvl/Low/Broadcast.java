b04c37fd2aa36b19052614d42b26564c5f7ac078
L1nc0ln <Lincoln-park@gmx.de>
2016-08-27 11:19:39 +0200

    java: brought class Broadcast to the intended version
    - the version that was in the LowLvl package was the up to date one,
      unfortunately I deleted that one

--- java-code/src/studyproject/API/Lvl/Low/Broadcast.java
+++ java-code/src/studyproject/API/Lvl/Low/Broadcast.java
@@ -8,37 +8,46 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.NoSuchElementException;
 
 import studyproject.API.Core.BroadcastInfo;
 import studyproject.API.Core.Utils;
 
 public class Broadcast {
 	
-	private static final int PORT = 9002;
-	private static final int PORT_SENDING = 9003;
-	private static final int BUFFERSIZE = 2048;
+	private static final int DEFAULT_PORT = 9002;
+	private static final int DEFAULT_PORT_SENDING = 9003;
+	private static final int DEFAULT_BUFFERSIZE = 2048;
+	
+	private static int broadcastPort = DEFAULT_PORT;
+	private static int outgoingPort = DEFAULT_PORT_SENDING;
+	private static int buffersize = DEFAULT_BUFFERSIZE;
 
 	/**
-	 * retrieves a list with all network addresses the local machine has
+	 * retrieves a list with all network addresses the local machine has that are running and not virtual
 	 * 
 	 * @param networkAddresses
 	 * 			the list in which the addresses are put into
 	 * 
 	 * @return
-	 * 			a list with all network addresses the local machine has
+	 * 			a list with all network addresses the local machine has that are running and not virtual
 	 */
 	public static int getNetworkAddresses(ArrayList<String> networkAddresses){
 		try{
 			Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
 			while(networkInterfaces.hasMoreElements()){
-				Enumeration<InetAddress> inetAddresses = networkInterfaces.nextElement().getInetAddresses();
-				while(inetAddresses.hasMoreElements()){
-					networkAddresses.add(inetAddresses.nextElement().getHostAddress());
+				NetworkInterface networkInterface = networkInterfaces.nextElement();
+				if(networkInterface.isUp() && !networkInterface.isVirtual()){
+					networkAddresses.add(networkInterface.getDisplayName());
 				}
+				
 			}
 		} catch(SocketException e){
 			//TODO real error codes
 			return -1;
+		} catch(NoSuchElementException f){
+			//TODO real error codes
+			return -4;
 		}
 		return 0;
 	}
@@ -60,19 +69,22 @@ public class Broadcast {
 	 * @param timestamp
 	 * 			the time of the last change in the tracked filesystem of this machine
 	 * 
+	 * @param load
+	 * 			how many bytes the client still has to send to other clients as part of a file transfer
+	 * 
 	 * @param name
 	 * 			the name that other machines should display this machine as
 	 * 
 	 * @return
 	 * 			0 or an error code
 	 */
-	public static int sendAdvertise(String broadcastAddress, String networkAddress, int ipPort, long timestamp, String name){
+	public static int sendAdvertise(String broadcastAddress, String networkAddress, int ipPort, long timestamp, long load, String name){
 	    DatagramPacket packet;
-	    try(DatagramSocket socket = new DatagramSocket(PORT_SENDING, InetAddress.getByName(networkAddress))){
+	    try(DatagramSocket socket = new DatagramSocket(outgoingPort, InetAddress.getByName(networkAddress))){
 	    	socket.setBroadcast(true);
-	    	byte[] packetContents = (networkAddress + " " + ipPort + " " + timestamp + " " + name + "\n").getBytes();
+	    	byte[] packetContents = (networkAddress + " " + ipPort + " " + timestamp + " " + load + " " + name + "\n").getBytes();
 	    	packet = new DatagramPacket(packetContents, packetContents.length,
-	    			InetAddress.getByName(broadcastAddress), PORT);
+	    			InetAddress.getByName(broadcastAddress), broadcastPort);
 	    	socket.send(packet);
 	    	//socket.disconnect();
 	    } catch(IOException e){
@@ -97,30 +109,95 @@ public class Broadcast {
 	 * 			0 or an error value
 	 */	
 	public static int readAdvertise(String broadcastAddress, BroadcastInfo broadcastInfo){
-		try(DatagramSocket socket = new DatagramSocket(PORT, InetAddress.getByName(broadcastAddress))){
-			DatagramPacket packet = new DatagramPacket(new byte[BUFFERSIZE], BUFFERSIZE);
+		try(DatagramSocket socket = new DatagramSocket(broadcastPort, InetAddress.getByName(broadcastAddress))){
+			DatagramPacket packet = new DatagramPacket(new byte[buffersize], buffersize);
 			socket.receive(packet);
 			int index = 0;
 			while(packet.getData()[index] != '\n'){
 				index++;
-				if(!(index < BUFFERSIZE)){
+				if(!(index < buffersize)){
 					return -2;
 				}
 			}
 			String packetContents = new String(Utils.getBytesFromTo(packet.getData(), 0, index), "UTF-8");
 			String[] packetParts = packetContents.split(" ");
-			if(packetParts.length != 4){
+			if(packetParts.length < 5){
 				//TODO real error codes
 				return -2;
 			}
 			broadcastInfo.networkAddress = packetParts[0];
 			broadcastInfo.ipPort = Integer.parseInt(packetParts[1]);
 			broadcastInfo.timestamp = Long.parseLong(packetParts[2]);
-			broadcastInfo.name = packetParts[3];
+			broadcastInfo.load = Long.parseLong(packetParts[3]);
+			String clientName = "";
+			clientName += packetParts[4];
+			for(int packetIndex = 5; packetIndex < packetParts.length; packetIndex++){
+				clientName += " " + packetParts[packetIndex];
+			}
+			broadcastInfo.name = clientName;
 		} catch(IOException e){
 			//TODO real error codes
 			return -1;
+		} catch(NumberFormatException f){
+			//TODO real error codes
+			return -3;
 		}
 		return 0;
 	}
+
+	/**
+	 * 
+	 * @return
+	 * 			the port the UDP packets are sent to
+	 */
+	public static int getBroadcastPort() {
+		return broadcastPort;
+	}
+
+	/**
+	 * 
+	 * @param broadcastPort
+	 * 			the port the UDP packets are sent to
+	 */
+	public static void setBroadcastPort(int broadcastPort) {
+		Broadcast.broadcastPort = broadcastPort;
+	}
+
+	/**
+	 * 
+	 * @return
+	 * 			the port the UDP socket is bound to locally
+	 */
+	public static int getOutgoingPort() {
+		return outgoingPort;
+	}
+
+	/**
+	 * 
+	 * @param outgoingPort
+	 * 			the port the UDP socket is bound to locally
+	 */
+	public static void setOutgoingPort(int outgoingPort) {
+		Broadcast.outgoingPort = outgoingPort;
+	}
+
+	/**
+	 * 
+	 * @return
+	 * 			the size of the buffer used to read data from the UDP packet
+	 */
+	public static int getBuffersize() {
+		return buffersize;
+	}
+
+	/**
+	 * 
+	 * @param buffersize
+	 * 			the size of the buffer used to read data from the UDP packet
+	 */
+	public static void setBuffersize(int buffersize) {
+		Broadcast.buffersize = buffersize;
+	}
+	
+	
 }
