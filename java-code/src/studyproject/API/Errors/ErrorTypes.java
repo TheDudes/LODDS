bd80e3323104a351cfeaac440ff6fbbfac6e8a76
Nintinugga <ninti@192.168.2.109>
2016-08-27 11:41:48 +0200

    java: doc: Added doc to ErrorTypes

--- java-code/src/studyproject/API/Errors/ErrorTypes.java
+++ java-code/src/studyproject/API/Errors/ErrorTypes.java
@@ -3,19 +3,36 @@ package studyproject.API.Errors;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Enum which describes an error and got an internal mapping of enum.ordinal()
+ * to enum to support ErrorTypes.valueOf(ErrorTypes.IO.ordinal()) which would
+ * return ErrorTypes.IO
+ * 
+ * @author ninti
+ *
+ */
 public enum ErrorTypes {
 	IO, blub, blah;
-	
 
-    private static Map<Integer, ErrorTypes> map = new HashMap<Integer, ErrorTypes>();
+	private static Map<Integer, ErrorTypes> map = new HashMap<Integer, ErrorTypes>();
 
-    static {
-        for (ErrorTypes error : ErrorTypes.values()) {
-            map.put(error.ordinal(), error);
-        }
-    }
+	static {
+		for (ErrorTypes error : ErrorTypes.values()) {
+			map.put(error.ordinal(), error);
+		}
+	}
 
-    public static ErrorTypes valueOf(int error) {
-        return map.get(error);
-    }
+	/**
+	 * Returns the ErrorTypes of the ordinal position, <b>shall only be called
+	 * with e.g ErrorTypes.IO.ordinal() as param
+	 * 
+	 * @param ordinalNr
+	 *            the ordinal number of the ErrorType you want to get e.g
+	 *            ErrorTypes.IO.ordinal()
+	 * @return The ErrorType on the ordinal position
+	 */
+	public static ErrorTypes valueOf(int ordinalNr) {
+		return map.get(ordinalNr);
+	}
+
 }
