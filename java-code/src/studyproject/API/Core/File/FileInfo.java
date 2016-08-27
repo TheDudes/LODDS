06cb0c5285e788b4aa7bbc597eb614e4309e6557
gitmalong <gitmalong@posteo.af>
2016-08-27 12:34:26 +0200

    java: class FileInfo has new constructor for auto determining file size and checksum

--- java-code/src/studyproject/API/Core/File/FileInfo.java
+++ java-code/src/studyproject/API/Core/File/FileInfo.java
@@ -1,8 +1,29 @@
 package studyproject.API.Core.File;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 public class FileInfo {
 	public String checksum;
 	public long size;
 	public String fileName;
 	public FileAction fileAction;
+	
+	public FileInfo(String checksum, long size, String fileName, FileAction fileAction) {
+		this.checksum = checksum;
+		this.size = size;
+		this.fileName = fileName;
+		this.fileAction = fileAction;
+	}
+	
+	public FileInfo(String fileName) throws NoSuchAlgorithmException, IOException {
+		File file = new File(fileName);
+		
+		this.checksum = FileHasher.getFileHash(fileName);
+		this.size = file.length();
+		this.fileName = fileName;
+		this.fileAction = FileAction.add;
+	}
+
 }
