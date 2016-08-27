e545c7e4a350639be3bad3bbd8b86fe539b280ce
Nintinugga <ninti@192.168.2.109>
2016-08-27 11:16:25 +0200

    java: doc: Added doc to Error.java

--- java-code/src/studyproject/API/Errors/Error.java
+++ java-code/src/studyproject/API/Errors/Error.java
@@ -6,16 +6,40 @@ import java.util.logging.LogRecord;
 import studyproject.logging.LogKey;
 import studyproject.logging.APILvl;
 
+/**
+ * java.util.logging {@link LogRecord} class which holds enhanced information
+ * about a log message like, {@link APILvl} and {@link LogKey}
+ * 
+ * @author ninti
+ *
+ */
 public class Error extends LogRecord {
 
 	private static final long serialVersionUID = 4997203555037544881L;
 	private LogKey logKey;
 	private APILvl apiLvl;
 
+	/**
+	 * @deprecated do not use this constructor
+	 * @param level
+	 * @param msg
+	 */
 	public Error(Level level, String msg) {
 		super(level, msg);
 	}
 
+	/**
+	 * Constructor to create a new instance of an Error LogRecord.
+	 * 
+	 * @param level
+	 *            the LogLevel {@link Level}
+	 * @param logKey
+	 *            the {@link LogKey}
+	 * @param apiLvl
+	 *            the {@link APILvl} where the Error is produced
+	 * @param msg
+	 *            the specific error message
+	 */
 	public Error(Level level, LogKey logKey, APILvl apiLvl, String msg) {
 		super(level, msg);
 		this.logKey = logKey;
