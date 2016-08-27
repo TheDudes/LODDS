b40560bcaf462f85525527ecb933901ea9e87d87
Nintinugga <ninti@192.168.2.109>
2016-08-27 11:42:24 +0200

    java: Added doc and Refactored ErrorHandler
    - getErrorMsgPrefix is now called in getErrorMsg

--- java-code/src/studyproject/API/Errors/ErrorHandler.java
+++ java-code/src/studyproject/API/Errors/ErrorHandler.java
@@ -6,22 +6,65 @@ import java.util.logging.Logger;
 import studyproject.logging.LogKey;
 import studyproject.logging.APILvl;
 
+/**
+ * Class to Handle Error codes and log them to the in the constructor overgiven
+ * {@link Logger}
+ * 
+ * @author ninti
+ *
+ */
 public class ErrorHandler {
 
 	private Logger logger;
 
+	/**
+	 * 
+	 * @param logger
+	 *            which shall log the logRecords
+	 */
 	public ErrorHandler(Logger logger) {
 		this.logger = logger;
 	}
 
+	/**
+	 * Creates a {@link Error} logs it to the in the constructor overgiven
+	 * {@link Logger} and returns the Object, will format the logRecord to
+	 * '{@link LogKey}: {@link APILvl} : Thrown By 'functionName': ErrorMessage'
+	 * 
+	 * @param level
+	 *            the LogLevel
+	 * @param logKey
+	 *            the LogKey
+	 * @param apiLvl
+	 *            the ApiLvl where the error was thrown
+	 * @param errorType
+	 *            The errorType to get the Error Message
+	 * @param functionName
+	 *            the function which have thrown the Error
+	 * @return the Error Object with filled information
+	 */
 	public Error logError(Level level, LogKey logKey, APILvl apiLvl, ErrorTypes errorType, String functionName) {
-		Error error = new Error(level, logKey, apiLvl,
-				getErrorMsg(errorType, getErrorMsgPrefix(logKey, apiLvl, functionName)));
+		Error error = new Error(level, logKey, apiLvl, getErrorMsg(errorType, logKey, apiLvl, functionName));
 		logger.log(error);
 		return error;
 	}
 
-	private static String getErrorMsg(ErrorTypes errorType, String errorMsgPrefix) {
+	/**
+	 * Uses the errorType to determine the ErrorMsg and format the determined
+	 * ErrorMsgString and errorMsgPrefix to 'errorMsgPrefix: errorMessage'
+	 * 
+	 * @param errorType
+	 *            The Type Of ErrorTypes to determine the error message
+	 * @param logKey
+	 *            the certein {@link LogKey}
+	 * @param apiLvl
+	 *            the {@link APILvl} where the error was produced
+	 * @param functionName
+	 *            the function which have thrown the error
+	 * @return a string formatted like '{@link LogKey}: {@link APILvl} :Thrown
+	 *         By 'functionName': ErrorMessage'
+	 */
+	private static String getErrorMsg(ErrorTypes errorType, LogKey logKey, APILvl apiLvl, String functionName) {
 		String errorMsg = "";
 		switch (errorType) {
 		case IO:
@@ -34,13 +77,23 @@ public class ErrorHandler {
 		default:
 			break;
 		}
-		return errorMsgPrefix + ": " + errorMsg;
+		return getErrorMsgPrefix(logKey, apiLvl, functionName) + ": " + errorMsg;
 
 	}
 
+	/**
+	 * 
+	 * @param logKey
+	 *            the certein {@link LogKey}
+	 * @param apiLvl
+	 *            the {@link APILvl} where the error was produced
+	 * @param functionName
+	 *            the function which have thrown the error
+	 * @return a string formatted like '{@link LogKey}: {@link APILvl} :Thrown
+	 *         By 'functionName'
+	 */
 	private static String getErrorMsgPrefix(LogKey logKey, APILvl apiLvl, String functionName) {
 		return logKey.toString() + ": " + apiLvl.toString() + ": Thrown By '" + functionName + "'";
 	}
-
 
 }
