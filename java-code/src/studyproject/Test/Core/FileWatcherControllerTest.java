6c7112c63b3332ea57abbbeaf274c3144d772234
gitmalong <gitmalong@posteo.af>
2016-09-17 16:35:17 +0200

    java: FileWatcherControllerTest, fix, adjusted to new test folder

--- java-code/src/studyproject/Test/Core/FileWatcherControllerTest.java
+++ java-code/src/studyproject/Test/Core/FileWatcherControllerTest.java
@@ -12,18 +12,20 @@ import studyproject.API.Core.File.FileInfo;
 import studyproject.API.Core.File.Watcher.FileWatcherController;
 
 public class FileWatcherControllerTest {
+	
+	private String testDirectory = "testData/FileWatcherController/";
 
 	@Test
 	public void listShouldContainOneFile() throws NoSuchAlgorithmException, IOException {
 		FileWatcherController controller = new FileWatcherController();
-		controller.watchDirectoryRecursively("/Users/robinhood/Desktop/testDirectory/oneFile");
+		controller.watchDirectoryRecursively(testDirectory+"oneFile");
 		assertEquals(1,controller.fileInfoList.size());
 	}
 	
 	@Test
 	public void listShouldContainTwoFiles() throws NoSuchAlgorithmException, IOException {
 		FileWatcherController controller = new FileWatcherController();
-		controller.watchDirectoryRecursively("/Users/robinhood/Desktop/testDirectory/twoFiles");
+		controller.watchDirectoryRecursively(testDirectory+"twoFiles");
 		assertEquals(2,controller.fileInfoList.size());
 	}
 	
@@ -46,7 +48,7 @@ public class FileWatcherControllerTest {
 	@Test
 	public void shouldGetCorrectFileInfoMessageWithTimestampZero() throws NoSuchAlgorithmException, IOException {
 		FileWatcherController controller = new FileWatcherController();
-		controller.watchDirectoryRecursively("/Users/robinhood/Desktop/testDirectory/oneFile");
+		controller.watchDirectoryRecursively(testDirectory+"oneFile");
 		
 		/**
 		 *  example output
@@ -56,10 +58,12 @@ public class FileWatcherControllerTest {
 		String actualResponse = controller.getInfo(0);
 		String expectedResponse = 
 				"all "+System.currentTimeMillis() / 1000L+" 1\n"
-				+ "add 736bf95996d40c71307e3727931b721dfb17bd27c441b903a6dd483b37021ac1 8 /Users/robinhood/Desktop/testDirectory/oneFile/testFile.txt\n";
+				+ "add 736bf95996d40c71307e3727931b721dfb17bd27c441b903a6dd483b37021ac1 8 "+testDirectory+"oneFile/testFile.txt\n";
 		
 		assertEquals(expectedResponse,actualResponse);
 	}
+	
+	
 	
 
 }
