8e2971bb4a15492be1d6af9cee59bba9aeea612f
Nintinugga <ninti@192.168.2.109>
2016-09-17 15:53:53 +0200

    java: changed Responses.java
    - put the static main in a function

--- java-code/src/studyproject/Test/Lvl/Low/Responses.java
+++ java-code/src/studyproject/Test/Lvl/Low/Responses.java
@@ -6,23 +6,30 @@ import studyproject.API.Core.File.FileAction;
 import studyproject.API.Core.File.FileInfo;
 
 public class Responses {
-	
-	public static void main(String[] args) {
+
+	private String pathToTestFile;
+
+	public Responses(String pathToTestFile) {
+		this.pathToTestFile = pathToTestFile;
+	}
+
+	public int test() {
+
 		try {
-			BufferedOutputStream socketStream = new BufferedOutputStream(new FileOutputStream("C:/Users/Chris/Documents/temp.txt"));
+			BufferedOutputStream socketStream = new BufferedOutputStream(new FileOutputStream(pathToTestFile));
 			long timestamp = System.currentTimeMillis();
 			ArrayList<FileInfo> fileInfos = new ArrayList<FileInfo>();
-			
-			BufferedReader inputStream = new BufferedReader(new FileReader("C:/Users/Chris/Documents/temp.txt"));
+
+			BufferedReader inputStream = new BufferedReader(new FileReader(pathToTestFile));
 			String str;
-			
+
 			fileInfos.add(new FileInfo("000001", 1000001, "asdf1.txt", FileAction.add));
 			fileInfos.add(new FileInfo("000002", 1000002, "asdf2.txt", FileAction.del));
 			fileInfos.add(new FileInfo("000003", 1000003, "asdf3.txt", FileAction.add));
-			
+
 			System.out.println("*** respondInfo called with timestamp being zero");
 			if (studyproject.API.Lvl.Low.Responses.respondInfo(socketStream, 0, fileInfos) == 0) {
-				while((str = inputStream.readLine()) != null) {
+				while ((str = inputStream.readLine()) != null) {
 					System.out.println(str);
 				}
 				System.out.println("reached end of file");
@@ -32,19 +39,22 @@ public class Responses {
 
 			System.out.println("*** respondInfo called with timestamp being NOT zero");
 			if (studyproject.API.Lvl.Low.Responses.respondInfo(socketStream, timestamp, fileInfos) == 0) {
-				while((str = inputStream.readLine()) != null) {
+				while ((str = inputStream.readLine()) != null) {
 					System.out.println(str);
 				}
 				System.out.println("reached end of file");
 			} else {
 				System.out.println("respondInfo failed");
 			}
-			
+
 			socketStream.close();
 			inputStream.close();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+
+		return 0;
 	}
+
 }
