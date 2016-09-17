b00e535b567166bb5691bf673f0978fd36efa1cd
L1nc0ln <Lincoln-park@gmx.de>
2016-09-17 18:30:28 +0200

    java: moved several variables from UserInfo to UserConnectionInfo

--- java-code/src/studyproject/API/Lvl/Mid/Core/UserConnectionInfo.java
+++ java-code/src/studyproject/API/Lvl/Mid/Core/UserConnectionInfo.java
@@ -5,26 +5,14 @@ import java.util.Vector;
 
 public class UserConnectionInfo extends UserInfo {
 
-	private long lastUpdate;
+
 	private long outstandingBytes;
-	private long load;
-	private Vector<RemoteFileInfo> fileList;
 	
-	public UserConnectionInfo(InetAddress ipAddress, int port, String userName, long lastUpdate,
-			long outstandingBytes, long load, Vector<RemoteFileInfo> fileList) {
-		super(ipAddress, port, userName);
-		this.lastUpdate = lastUpdate;
+	
+	public UserConnectionInfo(InetAddress ipAddress, int port, String userName, long lastUpdate, long load,
+			Vector<RemoteFileInfo> fileList, long outstandingBytes) {
+		super(ipAddress, port, userName, lastUpdate, load, fileList);
 		this.outstandingBytes = outstandingBytes;
-		this.load = load;
-		this.fileList = fileList;
-	}
-
-	public long getLastUpdate() {
-		return lastUpdate;
-	}
-
-	public void setLastUpdate(long lastUpdate) {
-		this.lastUpdate = lastUpdate;
 	}
 
 	public long getOutstandingBytes() {
@@ -35,22 +23,5 @@ public class UserConnectionInfo extends UserInfo {
 		this.outstandingBytes = outstandingBytes;
 	}
 
-	public long getLoad() {
-		return load;
-	}
-
-	public void setLoad(long load) {
-		this.load = load;
-	}
-
-	public Vector<RemoteFileInfo> getFileList() {
-		return fileList;
-	}
-
-	public void setFileList(Vector<RemoteFileInfo> fileList) {
-		this.fileList = fileList;
-	}
-	
-	
 
 }
--- java-code/src/studyproject/API/Lvl/Mid/Core/UserInfo.java
+++ java-code/src/studyproject/API/Lvl/Mid/Core/UserInfo.java
@@ -1,17 +1,25 @@
 package studyproject.API.Lvl.Mid.Core;
 
 import java.net.InetAddress;
+import java.util.Vector;
 
 public class UserInfo {
 
 	private InetAddress ipAddress;
 	private int port;
 	private String userName;
+	private long lastUpdate;
+	private long load;
+	private Vector<RemoteFileInfo> fileList;
 	
-	public UserInfo(InetAddress ipAddress, int port, String userName){
+	public UserInfo(InetAddress ipAddress, int port, String userName, long lastUpdate, long load,
+			Vector<RemoteFileInfo> fileList){
 		this.ipAddress = ipAddress;
 		this.port = port;
 		this.userName = userName;
+		this.lastUpdate = lastUpdate;
+		this.load = load;
+		this.fileList = fileList;
 	}
 	
 	public InetAddress getIpAddress() {
@@ -36,6 +44,30 @@ public class UserInfo {
 	
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public long getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(long lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public long getLoad() {
+		return load;
+	}
+
+	public void setLoad(long load) {
+		this.load = load;
+	}
+
+	public Vector<RemoteFileInfo> getFileList() {
+		return fileList;
+	}
+
+	public void setFileList(Vector<RemoteFileInfo> fileList) {
+		this.fileList = fileList;
 	}
 	
 }
