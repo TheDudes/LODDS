f7ad81230bcaeec32f7ee9b47fb08c04afe5c172
L1nc0ln <Lincoln-park@gmx.de>
2016-09-18 14:31:49 +0200

    java: BroadcastSenderThread now only uses LODDS object in constructor

--- java-code/src/studyproject/API/Lvl/Mid/BroadcastSenderThread.java
+++ java-code/src/studyproject/API/Lvl/Mid/BroadcastSenderThread.java
@@ -2,79 +2,27 @@ package studyproject.API.Lvl.Mid;
 
 import studyproject.API.Lvl.Low.Broadcast;
 
-public class BroadcastSenderThread extends Thread implements Runnable {
+public class BroadcastSenderThread extends Thread {
 	
 	
-	private String broadcastAddress;
-	private String networkAddress;
-	private int ipPort;
-	private String name;
 	private LODDS loddsObject;
-	private long timeInterval;
 	
-	public BroadcastSenderThread(String broadcastAddress, String networkAddress, int ipPort, String name,
-			LODDS loddsObject, long timeInterval) {
-		this.broadcastAddress = broadcastAddress;
-		this.networkAddress = networkAddress;
-		this.ipPort = ipPort;
-		this.name = name;
+	public BroadcastSenderThread(LODDS loddsObject) {
 		this.loddsObject = loddsObject;
-		this.timeInterval = timeInterval;
 	}
 
 	@Override
 	public void run() {
 		while(true){
 			try{
-				Broadcast.sendAdvertise(broadcastAddress, networkAddress, ipPort, loddsObject.getLastChange(),
-						loddsObject.getLoad(), name);
-				Thread.sleep(timeInterval);
+				Broadcast.sendAdvertise(loddsObject.getBroadcastAddress(), loddsObject.getNetworkAddress(),
+						loddsObject.getIpPort(), loddsObject.getLastChange(),
+						loddsObject.getLoad(), loddsObject.getUserName());
+				Thread.sleep(loddsObject.getTimeInterval());
 			} catch(InterruptedException e){
 				break;
 			}
 		}
 	}
-
-	public String getBroadcastAddress() {
-		return broadcastAddress;
-	}
-
-	public void setBroadcastAddress(String broadcastAddress) {
-		this.broadcastAddress = broadcastAddress;
-	}
-
-	public String getNetworkAddress() {
-		return networkAddress;
-	}
-
-	public void setNetworkAddress(String networkAddress) {
-		this.networkAddress = networkAddress;
-	}
-
-	public int getIpPort() {
-		return ipPort;
-	}
-
-	public void setIpPort(int ipPort) {
-		this.ipPort = ipPort;
-	}
-
-	public String getClientName() {
-		return name;
-	}
-
-	public void setClientName(String name) {
-		this.name = name;
-	}
-
-	public long getTimeInterval() {
-		return timeInterval;
-	}
-
-	public void setTimeInterval(long timeInterval) {
-		this.timeInterval = timeInterval;
-	}
-
-	
 	
 }
