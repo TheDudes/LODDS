1d7491cd0f5584753f33f1449e7aba0514dd21ba
L1nc0ln <Lincoln-park@gmx.de>
2016-09-18 10:37:07 +0200

    java: implemented all methods except start/stopAdvertising and Listening

--- java-code/src/studyproject/API/Lvl/Mid/LODDS.java
+++ java-code/src/studyproject/API/Lvl/Mid/LODDS.java
@@ -1,8 +1,12 @@
 package studyproject.API.Lvl.Mid;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Vector;
 
+import studyproject.API.Core.File.FileAction;
+import studyproject.API.Core.File.FileHasher;
 import studyproject.API.Lvl.Mid.Core.ConnectionInfo;
 import studyproject.API.Lvl.Mid.Core.FileChange;
 import studyproject.API.Lvl.Mid.Core.RemoteFileInfo;
@@ -10,13 +14,16 @@ import studyproject.API.Lvl.Mid.Core.UserInfo;
 
 public class LODDS {
 	
+	private long lastChange;
 	private Vector<FileChange> localFileChanges;
 	private Vector<UserInfo> clientList;
 	private Vector<RemoteFileInfo> availableFiles;
+	private Vector<String> sharedFolders; 
 	private long load;
 	private String interfaceName;
 	private int advertisePort;
 	private int listenPort;
+	@SuppressWarnings("unused")
 	private String userName;
 
 	public void startAdvertising(){
@@ -31,32 +38,85 @@ public class LODDS {
 	public void stopListening(){
 	}
 	
+	public void getFile(String user, String checksum, String localPath, int startIndex, int endIndex){
+		FileConnectionThread fileConnectionThread = new FileConnectionThread(getUserConnectionInfo(user),
+				checksum, getFileSize(checksum), localPath, startIndex, endIndex);
+		fileConnectionThread.run();
+	}
+	
 	public void getFile(String user, String checksum, String localPath){
-		
+		FileConnectionThread fileConnectionThread = new FileConnectionThread(getUserConnectionInfo(user),
+				checksum, getFileSize(checksum), localPath);
+		fileConnectionThread.run();
 	}
 	
 	public UserInfo getUserConnectionInfo(String user){
+		for(UserInfo currentUser: clientList){
+			if(currentUser.getUserName().equals(user));
+			return currentUser;
+		}
 		return null;
 	}
 	
 	public int shareFolder(String path){
-		return 0;
+		if(Files.exists(Paths.get(path)) && Files.isDirectory(Paths.get(path)) && ! sharedFolders.contains(path)){
+			sharedFolders.add(path);
+			//TODO tell the fileWatcher to start tracking this dir
+			try {
+				Files.walk(Paths.get(path)).forEach(filePath -> {
+					if (Files.isRegularFile(filePath)) {
+						try {
+							localFileChanges.add(new FileChange(path, Files.size(Paths.get(path)), FileHasher.getFileHash(path),
+									FileAction.add, System.currentTimeMillis() / 1000));
+						} catch (Exception e) {
+							//TODO what to do here?
+						}
+					}
+				});
+				setLastChange(System.currentTimeMillis() / 1000);
+				return 0;
+			} catch (IOException e) {
+				return 1;
+			}
+		}
+		return 4;
 	}
 	
 	public int unshareFolder(String path){
-		return 0;
+		if(sharedFolders.contains(path)){
+			sharedFolders.remove(path);
+			for(FileChange fileChange: localFileChanges){
+				if(fileChange.getPath().startsWith(path)){
+					localFileChanges.remove(fileChange);
+				}
+			}
+			setLastChange(System.currentTimeMillis() /1000);
+			//TODO tell fileWatcher to stop tracking this dir
+			return 0;
+		}
+		return 4;
 	}
 	
-	public ArrayList<String> getSharedFolders(){
-		return null;
+	public Vector<String> getSharedFolders(){
+		return sharedFolders;
 	}
 	
+	/**
+	 *  //TODO wtf did we think here?
+	 * @return
+	 */
 	public Vector<ConnectionInfo> getOutstandingBytes(){
 		return null;
 	}
 	
 	public Vector<FileChange> getFileChanges(long timeStamp){
-		return null;
+		Vector<FileChange> fileChanges = new Vector<FileChange>();
+		for(FileChange fileChange: localFileChanges){
+			if(fileChange.getTimeStamp() > timeStamp){
+				fileChanges.add(fileChange);
+			}
+		}
+		return fileChanges;
 	}
 	
 	public Vector<UserInfo> getUsers(){
@@ -89,6 +149,43 @@ public class LODDS {
 	
 	public void setUserName(String userName){
 		this.userName = userName;
+	}
+
+	/**
+	 * @return the lastChange
+	 */
+	public long getLastChange() {
+		return lastChange;
+	}
+
+	/**
+	 * @param lastChange the lastChange to set
+	 */
+	public void setLastChange(long lastChange) {
+		this.lastChange = lastChange;
+	}
+
+	/**
+	 * @return the load
+	 */
+	public long getLoad() {
+		return load;
+	}
+
+	/**
+	 * @param load the load to set
+	 */
+	public void setLoad(long load) {
+		this.load = load;
+	}
+	
+	private long getFileSize(String checksum){
+		for(RemoteFileInfo fileInfo: availableFiles){
+			if(fileInfo.getChecksum().equals(checksum)){
+				return fileInfo.getSize();
+			}
+		}
+		return 0;
 	}
 	
 }
