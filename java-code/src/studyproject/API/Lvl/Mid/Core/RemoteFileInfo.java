1d0fb1dc07a993d502526ea1c7411a3ca721ecfa
L1nc0ln <Lincoln-park@gmx.de>
2016-09-18 17:42:43 +0200

    java: added vector of users to RemoteFileInfo
    - the users are those users owning the file

--- java-code/src/studyproject/API/Lvl/Mid/Core/RemoteFileInfo.java
+++ java-code/src/studyproject/API/Lvl/Mid/Core/RemoteFileInfo.java
@@ -1,15 +1,19 @@
 package studyproject.API.Lvl.Mid.Core;
 
+import java.util.Vector;
+
 public class RemoteFileInfo {
 
 	private String path;
 	private long size;
 	private String checksum;
+	private Vector<UserInfo> owningUsers;
 	
 	public RemoteFileInfo(String path, long size, String checksum){
 		this.path = path;
 		this.size = size;
 		this.checksum = checksum;
+		owningUsers = new Vector<UserInfo>();
 	}
 	
 	public String getPath() {
@@ -34,6 +38,10 @@ public class RemoteFileInfo {
 	
 	public void setChecksum(String checksum) {
 		this.checksum = checksum;
+	}
+	
+	public Vector<UserInfo> getOwningUsers(){
+		return owningUsers;
 	}
 	
 	
