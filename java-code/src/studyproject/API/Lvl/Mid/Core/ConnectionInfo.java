49f274e26f68ba46c773b6a8c8e45a1e9e4a198a
L1nc0ln <Lincoln-park@gmx.de>
2016-09-20 13:32:12 +0200

    doc: added documentation to UserInfo and ConnectionInfo

--- java-code/src/studyproject/API/Lvl/Mid/Core/ConnectionInfo.java
+++ java-code/src/studyproject/API/Lvl/Mid/Core/ConnectionInfo.java
@@ -1,37 +1,74 @@
 package studyproject.API.Lvl.Mid.Core;
 
+/**
+ * class to store information about the state of a file transfer or other connection
+ * @author Michael
+ *
+ */
 public class ConnectionInfo {
 
 	private UserInfo userInfo;
 	private long outstandingBytes;
 	private RemoteFileInfo fileInfo;
 	
+	/**
+	 * 
+	 * @param userInfo
+	 * 			info about the user to which the connection is established
+	 * 
+	 * @param outstandingBytes
+	 * 			remaining bytes to transfer
+	 * 
+	 * @param fileInfo
+	 * 			info about the transmitted file
+	 */
 	public ConnectionInfo(UserInfo userInfo, long outstandingBytes, RemoteFileInfo fileInfo){
 		this.userInfo = userInfo;
 		this.outstandingBytes = outstandingBytes;
 		this.fileInfo = fileInfo;
 	}
 
+	/**
+	 * 
+	 * @return
+	 * 			the information about the other client
+	 */
 	public UserInfo getUserInfo() {
 		return userInfo;
 	}
 
+	/**
+	 * 
+	 * @param userInfo
+	 * 			information about the other client
+	 */
 	public void setUserInfo(UserInfo userInfo) {
 		this.userInfo = userInfo;
 	}
 
+	/**
+	 * 
+	 * @return
+	 * 			the number of bytes still to be transmitted
+	 */
 	public long getOutstandingBytes() {
 		return outstandingBytes;
 	}
 
-	public void setOutstandingBytes(long outstandingBytes) {
-		this.outstandingBytes = outstandingBytes;
-	}
-
+	/**
+	 * 
+	 * @return
+	 * 			information about the file being transmitted
+	 */
 	public RemoteFileInfo getFileInfo() {
 		return fileInfo;
 	}
 
+	/**
+	 * 
+	 * @param fileInfo
+	 * 			information about the file being transmitted
+	 */
 	public void setFileInfo(RemoteFileInfo fileInfo) {
 		this.fileInfo = fileInfo;
 	}
--- java-code/src/studyproject/API/Lvl/Mid/Core/UserInfo.java
+++ java-code/src/studyproject/API/Lvl/Mid/Core/UserInfo.java
@@ -3,6 +3,11 @@ package studyproject.API.Lvl.Mid.Core;
 import java.net.InetAddress;
 import java.util.Vector;
 
+/**
+ * class to store all important information about another user/client
+ * @author Michael
+ *
+ */
 public class UserInfo {
 
 	private InetAddress ipAddress;
@@ -14,6 +19,26 @@ public class UserInfo {
 	private long lastReceivedBroadcast;
 	private Vector<RemoteFileInfo> fileList;
 	
+	/**
+	 * 
+	 * @param ipAddress
+	 * 			the IP address of the other client
+	 * 
+	 * @param port
+	 * 			the port on which to contact the other client
+	 * 
+	 * @param userName
+	 * 			the name that the other client wants to be displayed as
+	 * 
+	 * @param lastUpdate
+	 * 			the time stamp of the last change in the file system of the other client
+	 * 
+	 * @param load
+	 * 			the number of bytes the other client needs to send
+	 * 
+	 * @param fileList
+	 * 			the list of files that the other client owns and shares
+	 */
 	public UserInfo(InetAddress ipAddress, int port, String userName, long lastUpdate, long load,
 			Vector<RemoteFileInfo> fileList){
 		this.ipAddress = ipAddress;
@@ -26,83 +51,170 @@ public class UserInfo {
 		lastReceivedBroadcast = 0;
 	}
 	
+	/**
+	 * 
+	 * @param ipAddress
+	 * 			the IP address of the other client
+	 * 
+	 * @param port
+	 * 			the port on which to contact the other client
+	 * 
+	 * @param userName
+	 * 			the name that the other client wants to be displayed as
+	 * 
+	 * @param lastUpdate
+	 * 			the time stamp of the last time the local client updated its files
+	 * 
+	 * @param load
+	 * 			the number of bytes the other client needs to send
+	 * 
+	 * @param fileList
+	 * 			the list of files that the other client owns and shares
+	 * 
+	 * @param lastReceivedBroadcast
+	 * 			the time of the last received broadcast of the other client
+	 */
 	public UserInfo(InetAddress ipAddress, int port, String userName, long lastUpdate, long load,
 			Vector<RemoteFileInfo> fileList, long lastReceivedBroadcast){
 		this(ipAddress, port, userName, lastUpdate, load, fileList);
 		this.lastReceivedBroadcast = lastReceivedBroadcast;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * 			the IP address of the other client
+	 */
 	public InetAddress getIpAddress() {
 		return ipAddress;
 	}
 	
+	/**
+	 * 
+	 * @param ipAddress
+	 * 			the ip address of the other client
+	 */
 	public void setIpAddress(InetAddress ipAddress) {
 		this.ipAddress = ipAddress;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * 			the port on which to contact the other client
+	 */
 	public int getPort() {
 		return port;
 	}
 	
+	/**
+	 * 
+	 * @param port
+	 * 			the port on which to contact the other client
+	 */
 	public void setPort(int port) {
 		this.port = port;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * 			the name that the other client wants to be displayed as
+	 */
 	public String getUserName() {
 		return userName;
 	}
 	
+	/**
+	 * 
+	 * @param userName
+	 * 			the name that the other client wants to be displayed as
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	/**
+	 * 
+	 * @return
+	 * 			the time stamp of the last time the local client updated its files
+	 */
 	public long getLastUpdate() {
 		return lastUpdate;
 	}
 
+	/**
+	 * 
+	 * @param lastUpdate
+	 * 			the time stamp of the last time the local client updated its files
+	 */
 	public void setLastUpdate(long lastUpdate) {
 		this.lastUpdate = lastUpdate;
 	}
 
+	/**
+	 * 
+	 * @return
+	 * 			the number of bytes the other client needs to send
+	 */
 	public long getLoad() {
 		return load;
 	}
 
+	/**
+	 * 
+	 * @param load
+	 * 			the number of bytes the other client needs to send
+	 */
 	public void setLoad(long load) {
 		this.load = load;
 	}
 
+	/**
+	 * 
+	 * @return
+	 * 			the list of files that the other client owns and shares
+	 */
 	public Vector<RemoteFileInfo> getFileList() {
 		return fileList;
 	}
 
+	/**
+	 * 
+	 * @param fileList
+	 * 			the list of files that the other client owns and shares
+	 */
 	public void setFileList(Vector<RemoteFileInfo> fileList) {
 		this.fileList = fileList;
 	}
 
 	/**
-	 * @return the fileListTimestamp
+	 * @return
+	 * 			the time stamp of the last change in the file system of the other client
 	 */
 	public long getFileListTimestamp() {
 		return fileListTimestamp;
 	}
 
 	/**
-	 * @param fileListTimestamp the fileListTimestamp to set
+	 * @param fileListTimestamp
+	 * 			the time stamp of the last change in the file system of the other client
 	 */
 	public void setFileListTimestamp(long fileListTimestamp) {
 		this.fileListTimestamp = fileListTimestamp;
 	}
 
 	/**
-	 * @return the lastReceivedBroadcast
+	 * @return
+	 * 			the time of the last received broadcast of the other client
 	 */
 	public long getLastReceivedBroadcast() {
 		return lastReceivedBroadcast;
 	}
 
 	/**
-	 * @param lastReceivedBroadcast the lastReceivedBroadcast to set
+	 * @param lastReceivedBroadcast
+	 * 			the time of the last received broadcast of the other client
 	 */
 	public void setLastReceivedBroadcast(long lastReceivedBroadcast) {
 		this.lastReceivedBroadcast = lastReceivedBroadcast;
