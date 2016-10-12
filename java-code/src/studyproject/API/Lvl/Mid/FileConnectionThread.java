29315c2c673f51312137a7c5ce933e72b9f9d0ec
Nintinugga <Jonas_Huber2@gmx.de>
2016-10-12 14:30:18 +0200

    java: Fixed fucked up git shit

--- java-code/src/studyproject/API/Lvl/Mid/FileConnectionThread.java
+++ java-code/src/studyproject/API/Lvl/Mid/FileConnectionThread.java
@@ -12,11 +12,10 @@ import studyproject.API.Lvl.Low.Requests;
 import studyproject.API.Lvl.Mid.Core.UserInfo;
 
 /**
- * Thread to get a file from another client
- * After creating a new instance of this class call
- * start() and the file specified in the constructor
- * will be transmitted in the way specified in the
- * specification
+ * Thread to get a file from another client After creating a new instance of
+ * this class call start() and the file specified in the constructor will be
+ * transmitted in the way specified in the specification
+ * 
  * @author Michael
  *
  */
@@ -28,56 +27,63 @@ public class FileConnectionThread extends Thread {
 	private long size;
 	private long startIndex = 0;
 	private long endIndex = 0;
-	
+
 	/**
-	 * Use this constructor if you want to pull the whole file,
-	 * if you only want to pull part of a file use the constructor
-	 * with start and endIndex
+	 * Use this constructor if you want to pull the whole file, if you only want
+	 * to pull part of a file use the constructor with start and endIndex
 	 * 
 	 * @param user
-	 * 			the user from which to pull the file
+	 *            the user from which to pull the file
 	 * 
 	 * @param checksum
-	 * 			the checksum of the file that should be pulled
+	 *            the checksum of the file that should be pulled
 	 * 
 	 * @param size
-	 * 			the size of the file that should be pulled
+	 *            the size of the file that should be pulled
 	 * 
 	 * @param localPath
-	 * 			the complete path on which the file should be saved
+	 *            the complete path on which the file should be saved
 	 */
+<<<<<<< Upstream, based on origin/master
 	public FileConnectionThread(UserInfo user, String checksum, long size, String localPath){
+=======
+	public FileConnectionThread(UserInfo user, String checksum, long size, String localPath, LODDS loddsObject) {
+>>>>>>> e7be19b java: Bugfix in run() of FileConnectionThread
 		this.user = user;
 		this.checksum = checksum;
 		this.localPath = localPath;
 		this.size = size;
 	}
-	
+
 	/**
 	 * 
-	 * This constructor is used if you only want to pull a part
-	 * of the file, the part is specified by the start and endIndex
+	 * This constructor is used if you only want to pull a part of the file, the
+	 * part is specified by the start and endIndex
 	 * 
 	 * @param user
-	 * 			the user from which to pull the file
+	 *            the user from which to pull the file
 	 * 
 	 * @param checksum
-	 * 			the checksum of the file that should be pulled
+	 *            the checksum of the file that should be pulled
 	 * 
 	 * @param size
-	 * 			the size of the file that should be pulled
+	 *            the size of the file that should be pulled
 	 * 
 	 * @param localPath
-	 * 			the complete path on which the file should be saved
+	 *            the complete path on which the file should be saved
 	 * 
 	 * @param startIndex
-	 * 			the index from which to start the transmission
+	 *            the index from which to start the transmission
 	 * 
 	 * @param endIndex
-	 * 			the index on which to end the transmission
+	 *            the index on which to end the transmission
 	 */
 	public FileConnectionThread(UserInfo user, String checksum, long size, String localPath, long startIndex,
+<<<<<<< Upstream, based on origin/master
 			long endIndex){
+=======
+			long endIndex, LODDS loddsObject) {
+>>>>>>> e7be19b java: Bugfix in run() of FileConnectionThread
 		this.user = user;
 		this.checksum = checksum;
 		this.localPath = localPath;
@@ -85,34 +91,53 @@ public class FileConnectionThread extends Thread {
 		this.endIndex = endIndex;
 		this.size = size;
 	}
-	
+
 	@Override
 	/**
 	 * starts pulling the file with the parameters set in the constructor
 	 */
 	public void run() {
 		int returnValue;
-		try(Socket socket = new Socket(user.getIpAddress(), user.getPort());
+		try (Socket socket = new Socket(user.getIpAddress(), user.getPort());
 				BufferedOutputStream outStream = new BufferedOutputStream(socket.getOutputStream());
 				BufferedInputStream inStream = new BufferedInputStream(socket.getInputStream());
-				FileOutputStream fileOutStream = new FileOutputStream(new File(localPath))){
-			if(endIndex == 0){
-				returnValue = Requests.getFile(outStream, checksum, startIndex, size - startIndex);
-			} else{
+				FileOutputStream fileOutStream = new FileOutputStream(new File(localPath))) {
+			//whole file?
+			if (endIndex == 0) {
+				returnValue = Requests.getFile(outStream, checksum, startIndex, size);
+			} else {
 				returnValue = Requests.getFile(outStream, checksum, startIndex, endIndex);
 			}
+<<<<<<< Upstream, based on origin/master
 			if(returnValue != 0){
 				//TODO error handling
+=======
+			if (returnValue != 0) {
+				// TODO error handling
+			} else {
+				loddsObject.incrementLoad(size);
+>>>>>>> e7be19b java: Bugfix in run() of FileConnectionThread
 			}
+<<<<<<< Upstream, based on origin/master
 			returnValue = Handles.handleFile(inStream, fileOutStream, endIndex - startIndex);
 			if(returnValue != 0){
 				//TODO error handling
+=======
+			//whole file?
+			if (endIndex == 0) {
+				returnValue = Handles.handleFile(inStream, fileOutStream, size);
+			} else {
+				returnValue = Handles.handleFile(inStream, fileOutStream, endIndex);
 			}
-		} catch(IOException e){
-			//TODO error handling
+			if (returnValue != 0) {
+				// TODO error handling
+			} else {
+				loddsObject.decrementLoad(size);
+>>>>>>> e7be19b java: Bugfix in run() of FileConnectionThread
+			}
+		} catch (IOException e) {
+			// TODO error handling
 		}
 	}
-
-	
 
 }
