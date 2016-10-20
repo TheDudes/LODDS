222478a7fa99be02bf515f25dc721b8ed523f45e
L1nc0ln <Lincoln-park@gmx.de>
2016-10-20 14:04:49 +0200

    java: added class to obtain the list of file changes from a client

--- java-code/src/studyproject/API/Lvl/Mid/UpdateFileInfoThread.java
+++ java-code/src/studyproject/API/Lvl/Mid/UpdateFileInfoThread.java
@@ -17,6 +17,15 @@ import studyproject.API.Lvl.Low.Requests;
 import studyproject.API.Lvl.Mid.Core.RemoteFileInfo;
 import studyproject.API.Lvl.Mid.Core.UserInfo;
 
+/**
+ * Thread that asks a specified user for an update to the list of shared files
+ * the user has. It then receives a list with changes and applies those to the
+ * list the user object has and to the hashmap of the lodds object that contains
+ * all shared files
+ * 
+ * @author Michael
+ *
+ */
 public class UpdateFileInfoThread extends Thread {
 
 	private UserInfo userConnectionInfo;
@@ -37,22 +46,55 @@ public class UpdateFileInfoThread extends Thread {
 			FileInfoListType infoType = new FileInfoListType();
 			Requests.getInfo(bufferedOutputStream, userConnectionInfo.getLastUpdate());
 			Handles.handleInfo(bufferedreader, fileInfoList, newFileListTimestamp, infoType);
-			insertEntries(fileInfoList);
+			userConnectionInfo.setFileListTimestamp(newFileListTimestamp.value);
+			userConnectionInfo.setLastUpdate(System.currentTimeMillis() / 1000);
+			updateHashmapEntries(fileInfoList);
+			updateClientFileList(fileInfoList);
 		} catch (IOException e) {
 			// TODO error handling
 		}
 	}
-	
-	private void insertEntries(ArrayList<FileInfo> fileInfoList){
+
+	/**
+	 * updates the list contained in the user object with the changes from the
+	 * list
+	 * 
+	 * @param fileInfoList
+	 *            the list of changes
+	 */
+	private void updateClientFileList(ArrayList<FileInfo> fileInfoList) {
+		for (FileInfo fileInfo : fileInfoList) {
+			if (fileInfo.fileAction.equals(FileAction.add)) {
+				userConnectionInfo.getFileList().add(new RemoteFileInfo(fileInfo));
+			} else if (fileInfo.fileAction.equals(FileAction.del)) {
+				userConnectionInfo.getFileList().remove(new RemoteFileInfo(fileInfo));
+			}
+		}
+	}
+
+	/**
+	 * applies the changes passed in the fileInfoList to the hashmap that
+	 * contains all shared files
+	 * 
+	 * @param fileInfoList
+	 *            the list of changes
+	 */
+	private void updateHashmapEntries(ArrayList<FileInfo> fileInfoList) {
 		ConcurrentHashMap<String, RemoteFileInfo> availableFiles = loddsObject.getAvailableFiles();
-		for(FileInfo fileInfo: fileInfoList){
-			if(fileInfo.fileAction.equals(FileAction.add)){
-				if(availableFiles.containsKey(fileInfo.checksum)){
-					if(!availableFiles.get(fileInfo.checksum).getOwningUsers()
-							.contains(userConnectionInfo)){
+		for (FileInfo fileInfo : fileInfoList) {
+			if (fileInfo.fileAction.equals(FileAction.add)) {
+				if (availableFiles.containsKey(fileInfo.checksum)) {
+					if (!availableFiles.get(fileInfo.checksum).getOwningUsers().contains(userConnectionInfo)) {
 						availableFiles.get(fileInfo.checksum).getOwningUsers().add(userConnectionInfo);
 					}
-				} else{
+				} else {
+					availableFiles.put(fileInfo.checksum, new RemoteFileInfo(fileInfo, userConnectionInfo));
+				}
+			} else if (fileInfo.fileAction.equals(FileAction.del)) {
+				if (availableFiles.get(fileInfo.checksum).getOwningUsers().size() > 1) {
+					availableFiles.get(fileInfo.checksum).getOwningUsers().remove(userConnectionInfo);
+				} else if (availableFiles.get(fileInfo.checksum).getOwningUsers().contains(userConnectionInfo)) {
+					availableFiles.remove(fileInfo.checksum);
 				}
 			}
 		}
