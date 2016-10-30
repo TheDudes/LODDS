af7d2520f8f009c8828d6e5e46fa73dd82ae2a67
d4ryus <d4ryus@openmailbox.org>
2016-10-30 20:18:15 +0100

    lisp: bugfix: fixed stream order on COPY-STREAM

--- cl-code/low-level-api.lisp
+++ cl-code/low-level-api.lisp
@@ -154,7 +154,7 @@ multiple-value-bind.
    file-stream will be positioned at start, and only transfer till end."
   (unless (eql start 0)
     (file-position file-stream start))
-  (lodds.core:copy-stream socket-stream file-stream (- end start))
+  (lodds.core:copy-stream file-stream socket-stream (- end start))
   0)
 
 (defun respond-info (socket-stream type timestamp file-infos)
