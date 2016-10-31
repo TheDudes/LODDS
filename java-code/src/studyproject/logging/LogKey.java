0dc34844a78074eaf71bffa1e142095f329599fc
Nintinugga <ninti@192.168.2.109>
2016-10-31 19:40:08 +0100

    java: updated new Logkeys to LogKey.java
    -added sendPermissionReceived, getSent, getReceived to LogKey.java
    
    - changed sendPermission to sendPermissionSent

--- java-code/src/studyproject/logging/LogKey.java
+++ java-code/src/studyproject/logging/LogKey.java
@@ -7,5 +7,5 @@ package studyproject.logging;
  *
  */
 public enum LogKey {
-	warning, error, filetransferComplete, filetransferInit, debug, broadcastSent, broadcastReceived, sendPermission
+ warning, error, filetransferComplete, filetransferInit, debug, broadcastSent, broadcastReceived, sendPermissionSent, sendPermissionReceived, getSent, getReceived;
 }
