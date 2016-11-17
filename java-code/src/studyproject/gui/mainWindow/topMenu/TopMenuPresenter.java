bf169f891e2a718165815001045074d83e2a363b
Nintinugga <Jonas_Huber2@gmx.de>
2016-11-17 16:49:45 +0100

    java: added shareFolder functionality to top Menu
    - opens an directory chooser, if the user chase one a new background
    thread is started to watch the file

--- java-code/src/studyproject/gui/mainWindow/topMenu/TopMenuPresenter.java
+++ java-code/src/studyproject/gui/mainWindow/topMenu/TopMenuPresenter.java
@@ -1,26 +1,62 @@
 package studyproject.gui.mainWindow.topMenu;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.inject.Inject;
+
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import studyproject.gui.mainWindow.MainWindowModel;
 import studyproject.gui.settingsWindow.SettingsWindowView;
 
 public class TopMenuPresenter implements Initializable {
 
 	public static Stage stage;
-	
+
 	@FXML
 	MenuItem settingsItem;
+	@FXML
+	MenuItem shareFolder;
+
+	private File chosenFolder;
+
+	@Inject
+	MainWindowModel mainWindowModel;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		settingsItem.setOnAction(e -> settingsItemPressed());
+		shareFolder.setOnAction(e -> shareFolderPressed());
+	}
+
+	private void shareFolderPressed() {
+		stage = new Stage();
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+
+		chosenFolder = directoryChooser.showDialog(stage);
+		stage.hide();
+		if (chosenFolder == null)
+			return;
+
+		Task<Void> shareFolderTask = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				mainWindowModel.getLodds().shareFolder(chosenFolder.getAbsolutePath());
+				return null;
+			}
+		};
+
+		Thread thread = new Thread(shareFolderTask);
+		thread.setDaemon(true);
+		thread.start();
 	}
 
 	public void settingsItemPressed() {
