774f90f8ea8d84dc682659798df512e9b8255e6e
chris <chris@L460.fritz.box>
2016-11-28 18:21:43 +0100

    java: Changed loadProperties
    Deleted old loadProperties(String s) function.
    Edited new loadProperties() function to load the properties from the
    properties file existing in the users home directory. If none is
    existing, create a new one and save the default properties in it

--- java-code/src/studyproject/App.java
+++ java-code/src/studyproject/App.java
@@ -1,14 +1,15 @@
 package studyproject;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.inject.Inject;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
@@ -22,17 +23,17 @@ import studyproject.logging.FileLogHandler;
 import studyproject.logging.LogConsoleHandler;
 import studyproject.logging.LogKey;
 
-public class App extends Application{
+public class App extends Application {
 
 	private static Logger logger;
-	public static Properties defaultProperties;
 	public static Properties properties;
-	public static String pathToProperties;
+	public static String pathToProperties = System.getProperty("user.home") + "/.lodds/config.properties";
 	private MainWindowView mainView;
 	private SelectedInterfaceView selectedInterfaceView;
-	
-	@Inject SelectedInterfaceModel selectedInterfaceModel;
-	
+
+	@Inject
+	SelectedInterfaceModel selectedInterfaceModel;
+
 	public void configureLogging() {
 		logger = Logger.getGlobal();
 		logger.setUseParentHandlers(false);
@@ -41,42 +42,36 @@ public class App extends Application{
 		logger.setLevel(Level.ALL);
 	}
 
-//	public int loadProperties(String pathToUserProperties) {
-//		properties = new Properties();
-//		
-//		try {
-//			properties.load(getClass().getResourceAsStream("resources/lodds.properties"));
-//		} catch (FileNotFoundException e) {
-//			return 4;
-//		} catch (IOException e) {
-//			return 4;
-//		}
-//		return 0;
-//	}
-	
+	/**
+	 * Create a properties file if not existent in the home directory of the user
+	 * Load default properties first then load properties changed by the user
+	 * 
+	 * @return ErrLog value
+	 */
 	public int loadProperties() {
-		defaultProperties = new Properties();
+		File propertiesFile = new File(pathToProperties);
 		properties = new Properties();
-		
 		try {
-			defaultProperties.load(getClass().getResourceAsStream("resources/lodds.properties"));
-			String pathToUserProperties = (String) properties.get("pathToUserProperties");
-			if (!pathToUserProperties.isEmpty()) {
-				properties.load(new FileInputStream(pathToUserProperties)); 
-			} else {
-				properties.load(getClass().getResourceAsStream("resources/lodds.properties"));
+			properties.load(getClass().getResourceAsStream("resources/lodds.properties"));
+			if (!propertiesFile.exists()) {
+				Files.createDirectories(Paths.get(propertiesFile.getParent()));
+				propertiesFile.getParentFile().mkdirs();
+				propertiesFile.createNewFile();
+				properties.store(new FileOutputStream(propertiesFile), null);
+				System.out.println("PropertiesFile created at " + propertiesFile.getAbsolutePath());
 			}
+			properties.load(new FileInputStream(propertiesFile));
+			// properties.store(new FileOutputStream(propertiesFile), null);
+			System.out.println("Using properties from " + propertiesFile.getAbsolutePath());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return 4;
+			return 1; // TODO check ErrLog value
 		}
-		
+
 		return 0;
 	}
-	
-	
-	
+
 	@Override
 	public void start(Stage mainStage) throws Exception {
 		selectedInterfaceView = new SelectedInterfaceView();
@@ -85,27 +80,22 @@ public class App extends Application{
 		interfaceStage.setScene(new Scene(selectedInterfaceView.getView()));
 		interfaceStage.initModality(Modality.APPLICATION_MODAL);
 		interfaceStage.showAndWait();
-		
+
 		mainStage.setTitle("Local Open Distributed Data Sharing");
 		mainView = new MainWindowView();
 		Scene mainScene = new Scene(mainView.getView());
 		mainStage.setScene(mainScene);
 		mainStage.show();
 	}
-	
 
 	public static void main(String... args) {
 		App application = new App();
-		pathToProperties = args[0];
 		int errorCode;
 		if ((errorCode = application.loadProperties()) > 0) {
-			ErrLog.log(Level.SEVERE, LogKey.error, APILvl.gui,errorCode, "loadProperties");
+			ErrLog.log(Level.SEVERE, LogKey.error, APILvl.gui, errorCode, "loadProperties");
 		}
 		application.configureLogging();
 		launch(args);
 
 	}
-	
-	
-	
 }
