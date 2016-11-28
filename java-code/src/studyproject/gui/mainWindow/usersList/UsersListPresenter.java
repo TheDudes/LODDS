864941d3e8c0f042daa9cf2caef1d863323f5d94
Nintinugga <ninti@192.168.2.109>
2016-11-28 10:55:37 +0100

    java: Added Controller Reference to UsersList.fxml
    - changed UsersListPresenter.java according to that

--- java-code/src/studyproject/gui/mainWindow/usersList/UsersListPresenter.java
+++ java-code/src/studyproject/gui/mainWindow/usersList/UsersListPresenter.java
@@ -5,6 +5,9 @@ import java.util.ResourceBundle;
 
 import javax.inject.Inject;
 
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,17 +15,36 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import studyproject.API.Lvl.Mid.Core.UserInfo;
+import studyproject.gui.mainWindow.MainWindowModel;
 
 public class UsersListPresenter implements Initializable{
 
 	@FXML TextField usersSearch;
-	@FXML ListView<UserInfo> usersListView;
+	@FXML ListView<UserInfo> usersListV;
 	@Inject UsersListModel userListsModel;
-	
+	@Inject MainWindowModel mainwindowModel;
+	private ObservableList<UserInfo> users;
+	protected ListProperty<UserInfo> listProperty = new SimpleListProperty<>();
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		ObservableList<UserInfo> users = FXCollections.observableArrayList();
+		users = FXCollections.observableArrayList(mainwindowModel.getLodds().getUsers());
+		
 		userListsModel.setUsers(users);
+		listProperty.set(users);
+		usersListV.itemsProperty().bind(listProperty);
+		//		usersListView.setCellFactory(param -> new ListCell<UserInfo>() {
+//		    @Override
+//		    protected void updateItem(UserInfo item, boolean empty) {
+//		        super.updateItem(item, empty);
+//
+//		        if (empty || item == null || item.getIpAddress() == null || item.getUserName() == null) {
+//		            setText(null);
+//		        } else {
+//		            setText(item.toString());
+//		        }
+//		    }
+//		});
+//		usersListView.setItems(userListsModel.getUsers());
 	}
 
 }
