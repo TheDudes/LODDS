a883115e05b3ce54af2194c2d964ddfc05bfa034
L1nc0ln <Lincoln-park@gmx.de>
2016-12-07 13:17:31 +0100

    java: moved creation and deletion of files to FileUtil

--- java-code/src/studyproject/Test/Lvl/Low/ResponsesTest.java
+++ java-code/src/studyproject/Test/Lvl/Low/ResponsesTest.java
@@ -3,152 +3,143 @@ package studyproject.Test.Lvl.Low;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import studyproject.API.Core.File.FileAction;
 import studyproject.API.Core.File.FileInfo;
 import studyproject.API.Lvl.Low.Responses;
+import studyproject.Test.FileUtil;
 
 public class ResponsesTest {
 
+	//private String readWritePath = "java-code/testData/Lvl.Low/readWriteFile.txt";
+	//private String readPath = "java-code/testData/Lvl.Low/readFile.txt";
+	//private String writePath = "java-code/testData/Lvl.Low/writeFile.txt";
+	private String readWritePath = "readWriteFile.txt";
+	private static String readPath = "readFile.txt";
+	private String writePath = "writeFile.txt";
+	
+	 @BeforeClass 
+	 public static void setUp() {
+		 FileUtil.createFile(FileUtil.fileContents2, readPath);
+	 }
+	 
+	 @AfterClass
+	 public static void cleanUp(){
+		 FileUtil.cleanUp();
+	 }
+
 	@Test
-	public void testRespondInfo() {
+	public void testRespondInfo() throws IOException {
 		System.out.println("*** testRespondInfo");
-		BufferedOutputStream socketStream;
 		long timestamp = System.currentTimeMillis();
 		ArrayList<FileInfo> fileInfos = new ArrayList<FileInfo>();
-		BufferedReader inputStream;
-		String readWritePath = "testData/Lvl.Low/readWriteFile.txt";
+		
+		FileUtil.createFile("", readWritePath);
 
-		try {
-			socketStream = new BufferedOutputStream(new FileOutputStream(
-					readWritePath));
-			inputStream = new BufferedReader(new FileReader(readWritePath));
-			fileInfos.add(new FileInfo("000001", 1000001, "asdf1.txt", "",
-					FileAction.add));
-			fileInfos.add(new FileInfo("000002", 1000002, "asdf2.txt", "",
-					FileAction.del));
+		try (BufferedOutputStream socketStream = new BufferedOutputStream(new FileOutputStream(FileUtil.dir + "/" + readWritePath));
+				BufferedReader inputStream = new BufferedReader(new FileReader(FileUtil.dir + "/" + readWritePath))) {
+
+			fileInfos.add(new FileInfo("000001", 1000001, "asdf1.txt", "", FileAction.add));
+			fileInfos.add(new FileInfo("000002", 1000002, "asdf2.txt", "", FileAction.del));
 			if (Responses.respondInfo(socketStream, 0, fileInfos) == 0) {
 				assertEquals("all 0 2", inputStream.readLine());
-				assertEquals("add 000001 1000001 asdf1.txt",
-						inputStream.readLine());
-				assertEquals("del 000002 1000002 asdf2.txt",
-						inputStream.readLine());
+				assertEquals("add 000001 1000001 asdf1.txt", inputStream.readLine());
+				assertEquals("del 000002 1000002 asdf2.txt", inputStream.readLine());
 			} else {
 				System.out.println("respondInfo failed");
+				fail();
 			}
-			socketStream.close();
-			inputStream.close();
-
-			fileInfos.clear();
-			Files.deleteIfExists(Paths.get(readWritePath));
-			Files.createFile(Paths.get(readWritePath));
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail();
 		}
+		
+		fileInfos.clear();
+		FileUtil.deleteFile(readWritePath);
+		FileUtil.createFile("", readWritePath);
 
-		try {
-			socketStream = new BufferedOutputStream(new FileOutputStream(
-					readWritePath));
-			inputStream = new BufferedReader(new FileReader(readWritePath));
-			fileInfos.add(new FileInfo("000003", 1000003, "FileWithoutDot", "",
-					FileAction.add));
+		try (BufferedOutputStream socketStream = new BufferedOutputStream(new FileOutputStream(FileUtil.dir + "/" + readWritePath));
+				BufferedReader inputStream = new BufferedReader(new FileReader(FileUtil.dir + "/" + readWritePath))) {
+			fileInfos.add(new FileInfo("000003", 1000003, "FileWithoutDot", "", FileAction.add));
 			if (Responses.respondInfo(socketStream, timestamp, fileInfos) == 0) {
 				assertEquals("upd " + timestamp + " 1", inputStream.readLine());
-				assertEquals("add 000003 1000003 FileWithoutDot",
-						inputStream.readLine());
+				assertEquals("add 000003 1000003 FileWithoutDot", inputStream.readLine());
 			} else {
 				System.out.println("respondInfo failed");
+				fail();
 			}
-			socketStream.close();
-			inputStream.close();
 
-			fileInfos.clear();
-			Files.deleteIfExists(Paths.get(readWritePath));
-			Files.createFile(Paths.get(readWritePath));
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail();
 		}
+		
+		fileInfos.clear();
+		FileUtil.deleteFile(readWritePath);
 	}
 
 	@Test
-	public void testRespondFile() {
+	public void testRespondFile() throws IOException {
 		System.out.println("*** testRespondFile");
-		BufferedOutputStream socketStream;
-		BufferedReader inputStream;
-		String readWritePath = "testData/Lvl.Low/readWriteFile.txt";
-		String readPath = "testData/Lvl.Low/readFile.txt";
 
-		try {
-			socketStream = new BufferedOutputStream(new FileOutputStream(
-					readWritePath));
-			inputStream = new BufferedReader(new FileReader(readWritePath));
-			FileInputStream fileInStream = new FileInputStream(readPath);
+		FileUtil.createFile("", readWritePath);
+		
+		try (BufferedOutputStream socketStream = new BufferedOutputStream(new FileOutputStream(FileUtil.dir + "/" + readWritePath));
+				BufferedReader inputStream = new BufferedReader(new FileReader(FileUtil.dir + "/" + readWritePath));
+				FileInputStream fileInStream = new FileInputStream(FileUtil.dir + "/" + readPath)){
+			
 			if (Responses.respondFile(socketStream, fileInStream, 0, 100) == 0) {
-				assertEquals(
-						"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, "
-								+ "sed diam nonumy eirmod tempor invidunt ut l",
-						inputStream.readLine());
+				assertEquals("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, "
+						+ "sed diam nonumy eirmod tempor invidunt ut l", inputStream.readLine());
 			}
-			fileInStream.close();
-			inputStream.close();
-			socketStream.close();
-			Files.deleteIfExists(Paths.get(readWritePath));
-			Files.createFile(Paths.get(readWritePath));
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail();
 		}
+		FileUtil.deleteFile(readWritePath);
 	}
 
 	@Test
-	public void testRespondSendPermission() {
+	public void testRespondSendPermission() throws IOException {
 		System.out.println("*** testRespondSendPermission");
-		String readWritePath = "testData/Lvl.Low/readWriteFile.txt";
-		String writePath = "testData/Lvl.Low/writeFile.txt";
-		String readPath = "testData/Lvl.Low/readFile.txt";
 
-		BufferedReader readWriteInStream;
-		BufferedReader writeInStream;
+		FileUtil.createFile("", readWritePath);
+		FileUtil.createFile("", writePath);
+		
+		SocketInheritor sockInheri = new SocketInheritor(FileUtil.dir + "/" + readWritePath, FileUtil.dir + "/" + readPath);
 
-		try {
-			SocketInheritor sockInheri = new SocketInheritor(readWritePath,
-					readPath);
-			FileOutputStream fileOutput = new FileOutputStream(writePath);
+		try (FileOutputStream fileOutput = new FileOutputStream(FileUtil.dir + "/" + writePath);
+				BufferedReader readWriteInStream = new BufferedReader(new FileReader(FileUtil.dir + "/" + readWritePath));
+				BufferedReader writeInStream = new BufferedReader(new FileReader(FileUtil.dir + "/" + writePath));){
+			
 			if (Responses.respondSendPermission(sockInheri, fileOutput, 100) == 0) {
-				fileOutput.close();
-				readWriteInStream = new BufferedReader(new FileReader(
-						readWritePath));
-				writeInStream = new BufferedReader(new FileReader(writePath));
+				
 
 				assertEquals("OK", readWriteInStream.readLine());
-				assertEquals(
-						"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, "
-								+ "sed diam nonumy eirmod tempor invidunt ut l",
-						writeInStream.readLine());
+				assertEquals("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, "
+						+ "sed diam nonumy eirmod tempor invidunt ut l", writeInStream.readLine());
+				
+				sockInheri.close();
 
-				readWriteInStream.close();
-				writeInStream.close();
-
-				Files.deleteIfExists(Paths.get(readWritePath));
-				Files.createFile(Paths.get(readWritePath));
-
-				Files.deleteIfExists(Paths.get(writePath));
-				Files.createFile(Paths.get(writePath));
-			} else {
-				fileOutput.close();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail();
 		}
+		
+		FileUtil.deleteFile(readWritePath);
+		FileUtil.deleteFile(writePath);
 	}
 
 }
