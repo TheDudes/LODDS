1e153f83fa30d1dc70c0c7d01fd55ce9313e8c9b
d4ryus <d4ryus@openmailbox.org>
2016-12-08 14:22:02 +0100

    lisp: moves request parsing to task
    Request parsing was done inside handler which blocked until message
    was received and parsed. It is now moved to a extra task which calls
    LOW-LEVEL-API:PARSE-REQUEST and then submits another task depending on
    request.

--- cl-code/handler.lisp
+++ cl-code/handler.lisp
@@ -1,28 +1,66 @@
 (in-package #:lodds.handler)
 
-(defmacro with-socket (socket &body body)
-  `(unwind-protect
-        (handler-case
-            (progn
-              (unless ,socket
-                (error "WTF WHY IS MY SOCKET NIL"))
-              ,@body)
-          (end-of-file ()
-            (lodds.event:push-event :error
-                                    (list :end-of-file)))
-          (error (e)
-            (lodds.event:push-event :error
-                                    (list e))))
-     (when ,socket
-       (close (usocket:socket-stream ,socket))
-       (usocket:socket-close ,socket))))
+(defmacro with-socket (socket close-socket-p &body body)
+  `(progn
+     (handler-case
+         (progn
+           (unless ,socket
+             (error "Could not access socket, nil."))
+           ,@body)
+       (end-of-file ()
+         (lodds.event:push-event :error
+                                 (list :end-of-file))
+         (when ,socket
+           (close (usocket:socket-stream ,socket))
+           (usocket:socket-close ,socket)))
+       (error (e)
+         (lodds.event:push-event :error
+                                 (list e))
+         (when ,socket
+           (close (usocket:socket-stream ,socket))
+           (usocket:socket-close ,socket))))
+     ,(when close-socket-p
+        `(when ,socket
+           (close (usocket:socket-stream ,socket))
+           (usocket:socket-close ,socket)))))
+
+(defmethod lodds.task:run-task ((task lodds.task:task-request))
+  (let ((socket (lodds.task:request-socket task)))
+    (with-socket socket nil
+      (multiple-value-bind (error request)
+          (lodds.low-level-api:parse-request (usocket:socket-stream socket))
+        (if (> error 0)
+            (error "low level api Returned error ~a~%" error)
+            (lodds.task:submit-task
+             (case (car request)
+               (:file
+                (destructuring-bind (checksum start end) (cdr request)
+                  (make-instance 'lodds.task:task-request-file
+                                 :name "request-file"
+                                 :request-socket socket
+                                 :request-checksum checksum
+                                 :request-start start
+                                 :request-end end)))
+               (:info
+                (make-instance 'lodds.task:task-request-info
+                               :name "request-info"
+                               :request-socket socket
+                               :request-timestamp (cadr request)))
+               (:send-permission
+                (destructuring-bind (size timeout filename) (cdr request)
+                  (make-instance 'lodds.task:task-request-send-permission
+                                 :name "request-send-permission"
+                                 :request-socket socket
+                                 :request-size size
+                                 :request-timeout timeout
+                                 :request-filename filename))))))))))
 
 (defmethod lodds.task:run-task ((task lodds.task:task-request-file))
   (with-accessors ((socket lodds.task:request-socket)
                    (checksum lodds.task:request-checksum)
                    (start lodds.task:request-start)
                    (end lodds.task:request-end)) task
-    (with-socket socket
+    (with-socket socket t
       (let ((filename (lodds.watcher:get-file-info checksum)))
         (if filename
             (with-open-file (file-stream filename
@@ -36,7 +74,7 @@
 (defmethod lodds.task:run-task ((task lodds.task:task-request-info))
   (with-accessors ((socket lodds.task:request-socket)
                    (timestamp lodds.task:request-timestamp)) task
-    (with-socket socket
+    (with-socket socket t
       (apply #'lodds.low-level-api:respond-info
              (usocket:socket-stream socket)
              (lodds:generate-info-response timestamp)))))
@@ -47,7 +85,7 @@
                    (timeout lodds.task:request-timeout)
                    (filename lodds.task:request-filename)) task
     (declare (ignore timeout))
-    (with-socket socket
+    (with-socket socket t
       ;; TODO: ask user here for file path
       (with-open-file (file-stream (concatenate 'string "/tmp/" filename)
                                    :direction :output
@@ -55,37 +93,6 @@
         (lodds.low-level-api:respond-send-permission (usocket:socket-stream socket)
                                                      file-stream
                                                      size)))))
-
-(defun handler-callback (socket)
-  "handles incomming connections and starts threads to handle
-  requests"
-  (multiple-value-bind (error request)
-      (lodds.low-level-api:parse-request (usocket:socket-stream socket))
-    (if (eql 0 error)
-        (lodds.task:submit-task
-          (case (car request)
-            (:file
-             (destructuring-bind (checksum start end) (cdr request)
-               (make-instance 'lodds.task:task-request-file
-                              :name "request-file"
-                              :request-socket socket
-                              :request-checksum checksum
-                              :request-start start
-                              :request-end end)))
-            (:info
-             (make-instance 'lodds.task:task-request-info
-                            :name "request-info"
-                            :request-socket socket
-                            :request-timestamp (cadr request)))
-            (:send-permission
-             (destructuring-bind (size timeout filename) (cdr request)
-               (make-instance 'lodds.task:task-request-send-permission
-                              :name "request-send-permission"
-                              :request-socket socket
-                              :request-size size
-                              :request-timeout timeout
-                              :request-filename filename)))))
-        (error "low level api Returned error ~a~%" error))))
 
 (defun run ()
   (let ((socket nil))
@@ -100,6 +107,9 @@
                                                         :timeout 1
                                                         :ready-only t)
                                 (eql :read (usocket:socket-state socket)))
-                       (handler-callback (usocket:socket-accept socket)))))
+                       (lodds.task:submit-task
+                        (make-instance 'lodds.task:task-request
+                                       :name "request"
+                                       :request-socket (usocket:socket-accept socket))))))
       (when socket
         (usocket:socket-close socket)))))
