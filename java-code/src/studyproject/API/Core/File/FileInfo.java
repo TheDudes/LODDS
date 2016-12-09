57e44335b89fcfc34b1ec1ba8fa36fdb85af5bb4
L1nc0ln <Lincoln-park@gmx.de>
2016-12-09 14:02:36 +0100

    java: fixed split throwing a PatternSyntaxException in windows
    - if the File.seperator is a \ in windows then the split is used with
    "\\\\" so there is no PatternSyntaxException
    - all other filesystems use the split(File.seperator)

--- java-code/src/studyproject/API/Core/File/FileInfo.java
+++ java-code/src/studyproject/API/Core/File/FileInfo.java
@@ -12,13 +12,15 @@ public class FileInfo {
 	public String relativeFileName; // unix format
 	public FileAction fileAction;
 	public String parentDirectory;
-	
+
 	/**
 	 * 
 	 * @param checksum
 	 * @param size
-	 * @param fileName: Absolute path in file system
-	 * @param virtualRoot: Virtual root directory with appending '/'
+	 * @param fileName:
+	 *            Absolute path in file system
+	 * @param virtualRoot:
+	 *            Virtual root directory with appending '/'
 	 * @param fileAction
 	 */
 	public FileInfo(String checksum, long size, String fileName, String virtualRoot, FileAction fileAction) {
@@ -27,22 +29,24 @@ public class FileInfo {
 		this.fileName = fileName;
 		this.fileAction = fileAction;
 		this.virtualRoot = virtualRoot;
-		
+
 		File file = new File(fileName);
 		this.parentDirectory = file.getParent();
 		this.relativeFileName = getRelativeFilePath();
 	}
-	
+
 	/**
 	 * 
-	 * @param fileName: Absolute path in file system
-	 * @param virtualRoot: Virtual root directory with appending '/'
+	 * @param fileName:
+	 *            Absolute path in file system
+	 * @param virtualRoot:
+	 *            Virtual root directory with appending '/'
 	 * @throws NoSuchAlgorithmException
 	 * @throws IOException
 	 */
 	public FileInfo(String fileName, String virtualRoot) throws NoSuchAlgorithmException, IOException {
 		File file = new File(fileName);
-		
+
 		this.checksum = FileHasher.getFileHash(fileName);
 		this.size = file.length();
 		this.fileName = fileName;
@@ -51,13 +55,18 @@ public class FileInfo {
 		this.virtualRoot = virtualRoot;
 		this.relativeFileName = getRelativeFilePath(); // unix
 	}
-	
+
 	private String getRelativeFilePath() {
-		String[] parentDirectorySplit = virtualRoot.split(File.separator);
-		String onlyParentDir = parentDirectorySplit[parentDirectorySplit.length-1];
-		return "/"+onlyParentDir+replaceBackslashWithForwardslash(fileName.replace(virtualRoot, ""));
+		String[] parentDirectorySplit;
+		if (File.separator.equals("\\")) {
+			parentDirectorySplit = virtualRoot.split("\\\\");
+		} else {
+			parentDirectorySplit = virtualRoot.split(File.separator);
+		}
+		String onlyParentDir = parentDirectorySplit[parentDirectorySplit.length - 1];
+		return "/" + onlyParentDir + replaceBackslashWithForwardslash(fileName.replace(virtualRoot, ""));
 	}
-	
+
 	private String replaceBackslashWithForwardslash(String fileName) {
 		return fileName.replace("\\", "/");
 	}
