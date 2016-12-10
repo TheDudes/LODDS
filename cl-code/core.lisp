b9ab18a5f800d7379c7e3aaa818f1f166a596f82
d4ryus <d4ryus@openmailbox.org>
2016-12-10 07:28:31 +0100

    lisp: bugfix: CORE:COPY-STREAM reading too much, element-type
    - fixes bug where copy-stream reads beyond given size
    - fixes copy-stream buffer type not beeing '(unsigned-byte 8)

--- cl-code/core.lisp
+++ cl-code/core.lisp
@@ -12,23 +12,34 @@
 (defun copy-stream (stream-from stream-to &optional size)
   "will read from stream-from and write to stream-to size bytes"
   (loop :with written = 0
-        :with buffer-size = 8192 ;; TODO: buffer-size to config
-        :with buffer = (make-array (list buffer-size))
+        :with buffer-size = (if (and size
+                                     (< size 8192))
+                                size
+                                8192)
+        :with buffer = (make-array (list buffer-size)
+                                   :element-type '(unsigned-byte 8))
         :for read = (read-sequence buffer stream-from)
         :until (zerop read)
         :do (progn
               (if (and size
-                       (> (+ read written)
-                          size))
+                       (>= (+ read written)
+                           size))
                   (progn
                     (write-sequence buffer stream-to :end (- size written))
                     ;; TODO: error handling
                     (return))
                   (progn
                     (write-sequence buffer stream-to :end read)
-                    (incf written read)))
+                    (incf written read)
+                    ;; in case the next read would be bigger then size
+                    (when (> buffer-size (- size written))
+                      ;; resize buffer to be fitting
+                      (setf buffer-size (- size written))
+                      (setf buffer (make-array (list buffer-size)
+                                               :element-type '(unsigned-byte 8))))))
               ;; TODO: error handling
-              (when (< read buffer-size) (return)))))
+              (when (< read buffer-size)
+                (return)))))
 
 ;; got code from lisptips http://lisptips.com/page/4 modified it to my needs :)
 
