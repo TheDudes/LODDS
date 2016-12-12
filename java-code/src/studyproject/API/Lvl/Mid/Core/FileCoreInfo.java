7bfea2152a1cf61de3d3f9c8869519039ce4280c
Nintinugga <Jonas_Huber2@gmx.de>
2016-12-12 11:08:43 +0100

    java: Fixed Hotfix of Filename in FileCoreInfo.java
    - the fileName is now the last part of the filePath split at "/"

--- java-code/src/studyproject/API/Lvl/Mid/Core/FileCoreInfo.java
+++ java-code/src/studyproject/API/Lvl/Mid/Core/FileCoreInfo.java
@@ -4,16 +4,18 @@ public class FileCoreInfo {
 
 	private String checksum;
 	private long filesize;
+	private String filePath;
 	private String fileName;
 
-	public FileCoreInfo(String checksum, long filesize, String fileName) {
+	public FileCoreInfo(String checksum, long filesize, String filePath) {
 		this.checksum = checksum;
 		this.filesize = filesize;
-		this.fileName = fileName;
+		this.filePath = filePath;
+		this.fileName = filePath.split("/")[(filePath.split("/")).length - 1];
 	}
 
 	public FileCoreInfo(String fileName) {
-		this.fileName = fileName;
+		this.filePath = fileName;
 	}
 
 	public String getChecksum() {
@@ -36,14 +38,21 @@ public class FileCoreInfo {
 		return fileName;
 	}
 
+	public String getFilePath() {
+		return filePath;
+	}
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
 
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
 	@Override
 	public String toString() {
-		//TODO this is not how it's supposed to be
-		return fileName.split("/")[(fileName.split("/")).length - 1];
+		return fileName;
 	}
 
 }
