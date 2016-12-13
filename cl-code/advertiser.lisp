dbd6774b90d7604451abb7d19e0840064be6f997
d4ryus <d4ryus@openmailbox.org>
2016-12-13 13:01:03 +0100

    lisp: adds better error handling when interface is not set

--- cl-code/advertiser.lisp
+++ cl-code/advertiser.lisp
@@ -7,26 +7,36 @@
   ;; TODO: this could fail
   (labels ((send ()
              (let ((interface (lodds:interface lodds:*server*)))
-               (lodds.low-level-api:send-advertise
-                (lodds:get-broadcast-address interface)
-                (lodds:broadcast-port lodds:*server*)
-                (list (lodds:get-ip-address interface)
-                      (lodds:handler-port lodds:*server*)
-                      (lodds:get-timestamp-last-change)
-                      (lodds:current-load lodds:*server*)
-                      (lodds:name lodds:*server*))))))
+               (unless interface
+                 (return-from send 8))
+               (let ((broadcast-address (lodds:get-broadcast-address interface)))
+                 (unless broadcast-address
+                   (return-from send 7))
+                 (lodds.low-level-api:send-advertise
+                  broadcast-address
+                  (lodds:broadcast-port lodds:*server*)
+                  (list (lodds:get-ip-address interface)
+                        (lodds:handler-port lodds:*server*)
+                        (lodds:get-timestamp-last-change)
+                        (lodds:current-load lodds:*server*)
+                        (lodds:name lodds:*server*)))))))
     (let ((result (send)))
-      (case result
-        ;; TODO: real error handling
-        (6 (restart-case (error "Network unreachable")
-             (retry-sending-advertise ()
-               (try-send))
-             (stop-advertising ()
-               nil)))
-        (0 (lodds.event:push-event :advertiser (list :sent)))
-        (t (error
-            "Unknown error occured in low-level-api (~a) on ADVERTISER"
-            result))))))
+      (restart-case
+          (case result
+            ;; TODO: real error handling
+            (8 (error "Interface not set"))
+            ;; TODO: real error handling
+            (7 (error "Could not read Broadcast Address"))
+            ;; TODO: real error handling
+            (6 (error "Network unreachable"))
+            (0 (lodds.event:push-event :advertiser (list :sent)))
+            (t (error
+                "Unknown error occured in low-level-api (~a) on ADVERTISER"
+                result)))
+        (retry-sending-advertise ()
+          (try-send))
+        (stop-advertising ()
+          nil)))))
 
 (defun run ()
   (loop :while (lodds.subsystem:alive-p (lodds:get-subsystem :advertiser))
