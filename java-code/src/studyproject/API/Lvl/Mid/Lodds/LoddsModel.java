0b0f69982724ce17dfee33a0084efc531ae50fa0
Tigerclaw1993 <christoph@muehlbauer.tk>
2017-02-10 17:03:41 +0100

    java: Added GetPermissionRequest-List to LoddsModel.java
    .

--- java-code/src/studyproject/API/Lvl/Mid/Lodds/LoddsModel.java
+++ java-code/src/studyproject/API/Lvl/Mid/Lodds/LoddsModel.java
@@ -4,34 +4,32 @@ import javax.annotation.PostConstruct;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import studyproject.API.Core.Request.GetPermissionRequest;
 import studyproject.API.Lvl.Mid.Core.UserInfo;
 import studyproject.API.Lvl.Mid.ThreadMonitoring.MonitoredThread;
 
 public class LoddsModel {
 	private ObservableList<UserInfo> clientList;
-
 	private ObservableList<MonitoredThread> tasksList;
+	private ObservableList<GetPermissionRequest> permissionList;
 
 	@PostConstruct
 	public void init() {
 		clientList = javafx.collections.FXCollections.synchronizedObservableList(FXCollections.observableArrayList());
 		tasksList = javafx.collections.FXCollections.synchronizedObservableList(FXCollections.observableArrayList());
+		permissionList = javafx.collections.FXCollections
+				.synchronizedObservableList(FXCollections.observableArrayList());
 	}
 
 	public synchronized ObservableList<MonitoredThread> getTasksList() {
 		return tasksList;
 	}
 
-	public synchronized void setTasksList(ObservableList<MonitoredThread> tasksList) {
-		this.tasksList = tasksList;
-	}
-
 	public synchronized ObservableList<UserInfo> getClientList() {
 		return clientList;
 	}
 
-	public synchronized void setClientList(ObservableList<UserInfo> clientList) {
-		this.clientList = clientList;
+	public synchronized ObservableList<GetPermissionRequest> getPermissionList() {
+		return permissionList;
 	}
-
 }
