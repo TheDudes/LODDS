867b55c18fb7f03dfa6750952d0264b4f9581d77
Nintinugga <jonas_huber2@gmx.de>
2017-02-10 15:26:10 +0100

    java: Fixed LogAreaHandler and -LogAreaPresenter auto scroll function
    - fixed auto scroll function if entries are deleted due to maximum size of the log message list
    
    - for this the virtual flow is extracted from the tableview's skin
    
    - the virtual flows function scrollToOffset is used if an entry is deleted

--- java-code/src/studyproject/gui/mainWindow/logArea/LogAreaPresenter.java
+++ java-code/src/studyproject/gui/mainWindow/logArea/LogAreaPresenter.java
@@ -23,122 +23,124 @@ import studyproject.API.Errors.Error;
 
 public class LogAreaPresenter implements Initializable {
 
-	@FXML
-	CheckMenuItem errorCB;
-	@FXML
-	CheckMenuItem infoCB;
-	@FXML
-	CheckMenuItem getRecCB;
-	@FXML
-	CheckMenuItem getSentCB;
-	@FXML
-	CheckMenuItem broadcastCB;
-	@FXML
-	TableView<Error> logTableView;
-	@FXML
-	TableColumn<Error, String> logKeyCol;
-	@FXML
-	TableColumn<Error, String> apiLvlCol;
-	@FXML
-	TableColumn<Error, String> thrownByCol;
-	@FXML
-	TableColumn<Error, String> msgCol;
-	@FXML
-	TableColumn<Error, String> timestampCol;
-	@FXML
-	TableColumn<Error, String> logLvlCol;
+    public static int NR_OF_ENTRANCES_TO_START_AUTOSCROLL = 10;
 
-	@Inject
-	LogAreaModel logAreaModel;
+    @FXML
+    CheckMenuItem errorCB;
+    @FXML
+    CheckMenuItem infoCB;
+    @FXML
+    CheckMenuItem getRecCB;
+    @FXML
+    CheckMenuItem getSentCB;
+    @FXML
+    CheckMenuItem broadcastCB;
+    @FXML
+    TableView<Error> logTableView;
+    @FXML
+    TableColumn<Error, String> logKeyCol;
+    @FXML
+    TableColumn<Error, String> apiLvlCol;
+    @FXML
+    TableColumn<Error, String> thrownByCol;
+    @FXML
+    TableColumn<Error, String> msgCol;
+    @FXML
+    TableColumn<Error, String> timestampCol;
+    @FXML
+    TableColumn<Error, String> logLvlCol;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		setLogAreaCheckBoxesSelectedProperties();
+    @Inject
+    LogAreaModel logAreaModel;
 
-		ObservableList<Error> errors = FXCollections.observableArrayList();
-		logTableView.setItems(errors);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setLogAreaCheckBoxesSelectedProperties();
 
-		addAutoScroll(logTableView);
+        ObservableList<Error> errors = FXCollections.observableArrayList();
+        logTableView.setItems(errors);
 
-		setTableColumnCellFactories();
+        addAutoScroll(logTableView);
 
-		bindSelectedPropertiesToModel();
+        setTableColumnCellFactories();
 
-		Logger.getGlobal().addHandler(new LogAreaHandler(Level.ALL, logTableView, logAreaModel));
+        bindSelectedPropertiesToModel();
 
-	}
+        Logger.getGlobal().addHandler(new LogAreaHandler(Level.ALL, logTableView, logAreaModel));
 
-	/**
-	 * Sets the selected properties of all Checkboxes(errorCB, infoCB, getRecCB,
-	 * getSentCB, broadcastCB) to the values parsed from App.properties
-	 */
-	private void setLogAreaCheckBoxesSelectedProperties() {
-		Platform.runLater(new Runnable() {
+    }
 
-			@Override
-			public void run() {
-				errorCB.setSelected(Boolean.valueOf(App.properties.getProperty("errorCB")));
-				infoCB.setSelected(Boolean.valueOf(App.properties.getProperty("infoCB")));
-				getRecCB.setSelected(Boolean.valueOf(App.properties.getProperty("getRecCB")));
-				getSentCB.setSelected(Boolean.valueOf(App.properties.getProperty("getSentCB")));
-				broadcastCB.setSelected(Boolean.valueOf(App.properties.getProperty("broadcastCB")));
+    /**
+     * Sets the selected properties of all Checkboxes(errorCB, infoCB, getRecCB,
+     * getSentCB, broadcastCB) to the values parsed from App.properties
+     */
+    private void setLogAreaCheckBoxesSelectedProperties() {
+        Platform.runLater(new Runnable() {
 
-			}
-		});
-	}
+            @Override
+            public void run() {
+                errorCB.setSelected(Boolean.valueOf(App.properties.getProperty("errorCB")));
+                infoCB.setSelected(Boolean.valueOf(App.properties.getProperty("infoCB")));
+                getRecCB.setSelected(Boolean.valueOf(App.properties.getProperty("getRecCB")));
+                getSentCB.setSelected(Boolean.valueOf(App.properties.getProperty("getSentCB")));
+                broadcastCB.setSelected(Boolean.valueOf(App.properties.getProperty("broadcastCB")));
 
-	/**
-	 * Binds the selected properties of all Checkboxes(errorCB, infoCB,
-	 * getRecCB, getSentCB, broadcastCB) to the corresponding
-	 * SimpleBooleanProperties of the logAreaModel
-	 */
-	private void bindSelectedPropertiesToModel() {
-		errorCB.selectedProperty().bindBidirectional(logAreaModel.getError());
-		infoCB.selectedProperty().bindBidirectional(logAreaModel.getInfo());
-		getRecCB.selectedProperty().bindBidirectional(logAreaModel.getGetRec());
-		getSentCB.selectedProperty().bindBidirectional(logAreaModel.getGetSent());
-		broadcastCB.selectedProperty().bindBidirectional(logAreaModel.getBroadcast());
-	}
+            }
+        });
+    }
 
-	/**
-	 * Sets all table column cell value factories
-	 */
-	private void setTableColumnCellFactories() {
-		timestampCol.setCellValueFactory(new PropertyValueFactory<Error, String>("timestamp"));
-		logKeyCol.setCellValueFactory(new PropertyValueFactory<Error, String>("logKey"));
-		apiLvlCol.setCellValueFactory(new PropertyValueFactory<Error, String>("apiLvl"));
-		thrownByCol.setCellValueFactory(new PropertyValueFactory<Error, String>("thrownBy"));
-		msgCol.setCellValueFactory(new PropertyValueFactory<Error, String>("msg"));
-		logLvlCol.setCellValueFactory(new PropertyValueFactory<Error, String>("logLevelString"));
-	}
+    /**
+     * Binds the selected properties of all Checkboxes(errorCB, infoCB,
+     * getRecCB, getSentCB, broadcastCB) to the corresponding
+     * SimpleBooleanProperties of the logAreaModel
+     */
+    private void bindSelectedPropertiesToModel() {
+        errorCB.selectedProperty().bindBidirectional(logAreaModel.getError());
+        infoCB.selectedProperty().bindBidirectional(logAreaModel.getInfo());
+        getRecCB.selectedProperty().bindBidirectional(logAreaModel.getGetRec());
+        getSentCB.selectedProperty().bindBidirectional(logAreaModel.getGetSent());
+        broadcastCB.selectedProperty().bindBidirectional(logAreaModel.getBroadcast());
+    }
 
-	/**
-	 * Adds an auto scroll property to the in the parameter list passed
-	 * TableView, which causes the TableView to automatically scroll down if an
-	 * entry is made and the scroll bar is at the bottom of the view
-	 * 
-	 * @param view
-	 *            The TableView where the auto scroll functionality shall be
-	 *            added
-	 */
-	public static <S> void addAutoScroll(final TableView<S> view) {
-		if (view == null) {
-			throw new NullPointerException();
-		}
+    /**
+     * Sets all table column cell value factories
+     */
+    private void setTableColumnCellFactories() {
+        timestampCol.setCellValueFactory(new PropertyValueFactory<Error, String>("timestamp"));
+        logKeyCol.setCellValueFactory(new PropertyValueFactory<Error, String>("logKey"));
+        apiLvlCol.setCellValueFactory(new PropertyValueFactory<Error, String>("apiLvl"));
+        thrownByCol.setCellValueFactory(new PropertyValueFactory<Error, String>("thrownBy"));
+        msgCol.setCellValueFactory(new PropertyValueFactory<Error, String>("msg"));
+        logLvlCol.setCellValueFactory(new PropertyValueFactory<Error, String>("logLevelString"));
+    }
 
-		view.getItems().addListener((ListChangeListener<S>) (c -> {
-			c.next();
-			final ScrollBar scrollBar = (ScrollBar) view.lookup(".scroll-bar:vertical");
-			final int size = view.getItems().size();
-			if ((size > 0 && (scrollBar.valueProperty().get() >= scrollBar.getMax())) || size > 0 && size < 20) {
-				Platform.runLater(new Runnable() {
-					@Override
-					public void run() {
-						view.scrollTo(size - 1);
-					}
-				});
-			}
-		}));
-	}
+    /**
+     * Adds an auto scroll property to the in the parameter list passed
+     * TableView, which causes the TableView to automatically scroll down if an
+     * entry is made and the scroll bar is at the bottom of the view
+     *
+     * @param view The TableView where the auto scroll functionality shall be
+     *             added
+     */
+    public static <S> void addAutoScroll(final TableView<S> view) {
+        if (view == null) {
+            throw new NullPointerException();
+        }
+
+        view.getItems().addListener((ListChangeListener<S>) (c -> {
+            c.next();
+            final ScrollBar scrollBar = (ScrollBar) view.lookup(".scroll-bar:vertical");
+            final int size = view.getItems().size();
+            if ((size > 0 && (scrollBar.valueProperty().get() >= scrollBar.getMax()))
+                    || (size > 0) && (size < NR_OF_ENTRANCES_TO_START_AUTOSCROLL)) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        view.scrollTo(size - 1);
+                    }
+                });
+            }
+        }));
+    }
 
 }
