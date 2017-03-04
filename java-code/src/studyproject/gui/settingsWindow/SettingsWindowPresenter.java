4be11197daf55954843519499c4369ae10f283f0
Tigerclaw1993 <christoph@muehlbauer.tk>
2017-03-04 12:45:34 +0100

    java: removed warning
    - unused import

--- java-code/src/studyproject/gui/settingsWindow/SettingsWindowPresenter.java
+++ java-code/src/studyproject/gui/settingsWindow/SettingsWindowPresenter.java
@@ -19,7 +19,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import studyproject.API.Errors.ErrorFactory;
