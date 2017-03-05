01b315819233210cf176ae37ff5e91c0ce6cd4c0
d4ryus <d4ryus@openmailbox.org>
2017-03-05 18:56:32 +0100

    tools: adds tool to generate icons

---
+++ tools/generate-icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+convert ../lodds.svg ../java-code/src/studyproject/resources/lodds_icon16x16.png 
+convert ../lodds.svg ../java-code/src/studyproject/resources/lodds_icon32x32.png 
+convert ../lodds.svg ../java-code/src/studyproject/resources/lodds_icon64x64.png 
