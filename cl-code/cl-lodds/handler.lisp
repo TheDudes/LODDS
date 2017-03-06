204e5a976762527aa55eb3e34a234058d7662141
d4ryus <d4ryus@openmailbox.org>
2017-03-06 21:44:04 +0100

    lisp: fixes handler error on startup
    If handler cant start due to a error, it now just retries after a
    second until it either works or handler gets stopped.

--- cl-code/cl-lodds/handler.lisp
+++ cl-code/cl-lodds/handler.lisp
@@ -1,26 +1,36 @@
 (in-package #:lodds.handler)
 
+(defun open-socket ()
+  (handler-case
+      (usocket:socket-listen
+       (lodds.core:get-ip-address (lodds.config:get-value :interface))
+       (lodds.config:get-value :port)
+       :reuse-address t
+       :element-type '(unsigned-byte 8))
+    (error (e)
+      (lodds.event:push-event :error
+                              (list (format nil
+                                            "Error starting Listener (~a)"
+                                            e)))
+      (sleep 1)
+      (when (lodds.subsystem:alive-p (lodds:get-subsystem :handler))
+        (open-socket)))))
+
 (defun run ()
-  (let ((socket nil))
+  (let ((socket (open-socket)))
     (unwind-protect
-         (progn
-           (setf socket (usocket:socket-listen
-                         (lodds.core:get-ip-address (lodds.config:get-value :interface))
-                         (lodds.config:get-value :port)
-                         :reuse-address t
-                         :element-type '(unsigned-byte 8)))
-           (loop :while (lodds.subsystem:alive-p (lodds:get-subsystem :handler))
-                 :do (when (and (lodds.core:input-rdy-p socket 1)
-                                (eql :read (usocket:socket-state socket)))
-                       (lodds.task:submit-task
-                        (make-instance 'lodds.task:task-request
-                                       :name "request"
-                                       :socket (let ((socket
-                                                       (usocket:socket-accept socket
-                                                                              :element-type '(unsigned-byte 8))))
-                                                 ;; TODO: move specified timeout to settings
-                                                 (setf (usocket:socket-option socket :receive-timeout) 1)
-                                                 socket))))))
+         (loop :while (lodds.subsystem:alive-p (lodds:get-subsystem :handler))
+               :do (when (and (lodds.core:input-rdy-p socket 1)
+                              (eql :read (usocket:socket-state socket)))
+                     (lodds.task:submit-task
+                      (make-instance 'lodds.task:task-request
+                                     :name "request"
+                                     :socket
+                                     (let ((client-socket
+                                             (usocket:socket-accept socket
+                                                                    :element-type '(unsigned-byte 8))))
+                                       ;; TODO: move specified timeout to settings
+                                       (setf (usocket:socket-option client-socket :receive-timeout) 1)
+                                       client-socket)))))
       (when socket
         (usocket:socket-close socket)))))
-
