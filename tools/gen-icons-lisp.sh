07bdfc33ab76fc6b925160de9dd447fbcf5955ff
d4ryus <d4ryus@openmailbox.org>
2017-03-13 19:03:17 +0100

    lisp: background has to go first, or it gets ignored :/

--- tools/gen-icons-lisp.sh
+++ tools/gen-icons-lisp.sh
@@ -3,11 +3,11 @@
 # Generates png icons needed for the cl-lodds-qt client.
 # convert is part of ImageMagick
 
-convert ../res/lodds.svg         -background transparent -resize 64x64 ../cl-code/res/file.png
-convert ../res/file.svg          -background transparent -resize 64x64 ../cl-code/res/file.png
-convert ../res/folder-closed.svg -background transparent -resize 64x64 ../cl-code/res/folder-closed.png
-convert ../res/folder-open.svg   -background transparent -resize 64x64 ../cl-code/res/folder-open.png
-convert ../res/send-file.svg     -background transparent -resize 64x64 ../cl-code/res/send-file.png
-convert ../res/shared.svg        -background transparent -resize 46x64 ../cl-code/res/shared.png
-convert ../res/user.svg          -background transparent -resize 46x64 ../cl-code/res/user.png
-convert ../res/x.svg             -background transparent -resize 64x64 ../cl-code/res/x.png
+convert -background transparent ../res/lodds.svg         -resize 64x64 ../cl-code/res/lodds.png
+convert -background transparent ../res/file.svg          -resize 64x64 ../cl-code/res/file.png
+convert -background transparent ../res/folder-closed.svg -resize 64x64 ../cl-code/res/folder-closed.png
+convert -background transparent ../res/folder-open.svg   -resize 64x64 ../cl-code/res/folder-open.png
+convert -background transparent ../res/send-file.svg     -resize 64x64 ../cl-code/res/send-file.png
+convert -background transparent ../res/shared.svg        -resize 46x64 ../cl-code/res/shared.png
+convert -background transparent ../res/user.svg          -resize 46x64 ../cl-code/res/user.png
+convert -background transparent ../res/x.svg             -resize 64x64 ../cl-code/res/x.png
