b7750eb435f20b07637b322230c1be031822fb34
gitmalong <gitmalong@posteo.af>
2017-03-17 14:31:04 +0100

    java: generate-icons generates only one lodds icon
    - before it generated pngs in various resolutions

--- tools/generate-icons.sh
+++ tools/generate-icons.sh
@@ -18,19 +18,8 @@ svg_dir=${script_dir}../res/
 java_res_dir=${script_dir}../java-code/src/studyproject/resources/lodds_icon/
 mkdir -p java_res_dir
 
-# Convert lodds icon in various solution
-svg=${svg_dir}lodds.svg
-lodds_prefix=${java_res_dir}lodds_icon
-
-resolutions=(16x16 32x32 64x64)
-for i in "${resolutions[@]}"
-do
-: 
-   convert -background transparent -resize $i $svg ${lodds_prefix}${i}.png
-done
-
-# Convert other svg files
-svgs=(file folder-closed folder-open reload send-file shared user x)
+# Convert svg files
+svgs=(lodds file folder-closed folder-open reload send-file shared user x)
 for i in "${svgs[@]}"
 do
 : 
